69e1d100a1d6b076fea72b01ff2d07b70db4369e
Maksym Poliakov <maxpoliakov.git@gmail.com>
2019-09-25 22:08:37 +0300

    Add plus button to action bar, create AddActivity and RecordActivity fragments

--- app/src/main/java/com/example/timeo/MainActivity.kt
+++ app/src/main/java/com/example/timeo/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.timeo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import androidx.navigation.ui.NavigationUI
@@ -25,5 +26,10 @@ class MainActivity : AppCompatActivity() {
 
     override fun onSupportNavigateUp(): Boolean {
         return NavigationUI.navigateUp(navController, null)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.action_bar_menu, menu)
+        return true
     }
 }

6ab8b45b695dfe2a793b0ce93cd2eb1831b1a605
Maksym Poliakov <maxpoliakov.git@gmail.com>
2019-10-02 22:47:41 +0300

    Add DataManipulator class, create basic activities recording

--- app/src/main/java/com/jdevs/timeo/AddActivityFragment.kt
+++ app/src/main/java/com/jdevs/timeo/AddActivityFragment.kt
@@ -1,24 +1,33 @@
 package com.jdevs.timeo
 
-
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import kotlin.reflect.KProperty
+import android.widget.Toast
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 
 class AddActivityFragment : DataManipulatorFragment() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        Records.addActivity(context, "Programming", "Laptop.png")
+        val activities = Records.listActivities(context)
+
+//        Iterate over activities
+        for (activity in activities) {
+            Toast.makeText(context, activity.name, Toast.LENGTH_LONG).show()
+            Toast.makeText(context, "", Toast.LENGTH_SHORT).show()
+        }
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-//        Records.writeFile()
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_add_activity, container, false)
     }

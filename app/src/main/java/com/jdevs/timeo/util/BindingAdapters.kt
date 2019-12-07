a00ac01eaaa43b69f66fa7570a1a098b51ee9e38
Maksym Poliakov <maxpoliakov.git@gmail.com>
2019-12-07 19:29:13 +0200

    Refactor BindingAdapters.kt, remove useless functionality refactor XML resources

--- app/src/main/java/com/jdevs/timeo/util/BindingAdapters.kt
+++ app/src/main/java/com/jdevs/timeo/util/BindingAdapters.kt
@@ -1,16 +1,9 @@
 package com.jdevs.timeo.util
 
-import android.graphics.Color
-import android.text.SpannableString
-import android.text.Spanned
-import android.text.style.ForegroundColorSpan
 import android.view.KeyEvent
-import android.view.MotionEvent
 import android.view.View
 import android.view.inputmethod.EditorInfo
 import android.widget.EditText
-import android.widget.TextView
-import androidx.core.content.ContextCompat
 import androidx.databinding.BindingAdapter
 import com.google.android.gms.common.SignInButton
 import com.google.android.material.textfield.TextInputLayout
@@ -19,54 +12,6 @@ import com.google.android.material.textfield.TextInputLayout
 fun hideIf(view: View, shouldHide: Boolean) {
 
     view.visibility = if (shouldHide) View.GONE else View.VISIBLE
-}
-
-@BindingAdapter("clickableStyle")
-fun applyClickableStyle(view: TextView, value: Boolean) {
-
-    if (!value) {
-        return
-    }
-
-    view.apply {
-        val signupText = text
-
-        val notClickedString = SpannableString(signupText)
-
-        notClickedString.setSpan(
-            ForegroundColorSpan(
-                ContextCompat.getColor(context, android.R.color.holo_blue_dark)
-            ), 0, notClickedString.length, 0
-        )
-
-        setText(notClickedString, TextView.BufferType.SPANNABLE)
-
-        val clickedString = SpannableString(notClickedString)
-        clickedString.setSpan(
-            ForegroundColorSpan(Color.BLUE), 0, notClickedString.length,
-            Spanned.SPAN_INCLUSIVE_EXCLUSIVE
-        )
-
-        setOnTouchListener { v, event ->
-
-            when (event.action) {
-
-                MotionEvent.ACTION_DOWN -> text = clickedString
-
-                MotionEvent.ACTION_UP -> {
-                    setText(notClickedString, TextView.BufferType.SPANNABLE)
-                    v.performClick()
-                }
-
-                MotionEvent.ACTION_CANCEL -> setText(
-                    notClickedString,
-                    TextView.BufferType.SPANNABLE
-                )
-            }
-
-            true
-        }
-    }
 }
 
 @BindingAdapter("error", "editText", requireAll = true)
@@ -81,6 +26,7 @@ fun setError(textInputLayout: TextInputLayout, error: String, editText: EditText
     textInputLayout.error = error
 
     editText.apply {
+
         requestFocus()
         setSelection(length())
 
@@ -107,7 +53,7 @@ fun setOnEnterPressedListener(view: View, block: Runnable) {
 }
 
 @BindingAdapter("android:onClick")
-fun bindSignInClick(button: SignInButton, block: Runnable) {
+fun setOnClickListener(button: SignInButton, block: Runnable) {
 
     button.setOnClickListener { block.run() }
 }

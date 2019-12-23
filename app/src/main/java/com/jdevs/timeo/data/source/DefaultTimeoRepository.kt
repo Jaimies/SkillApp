e11f0af683121fee8abdde738921f6fe9b14d92f
Maksym Poliakov <maxpoliakov.git@gmail.com>
2019-12-23 22:31:55 +0200

    Make DefaultTimeoRepository.kt less complicated

--- app/src/main/java/com/jdevs/timeo/data/source/DefaultTimeoRepository.kt
+++ app/src/main/java/com/jdevs/timeo/data/source/DefaultTimeoRepository.kt
@@ -9,71 +9,45 @@ class DefaultTimeoRepository(
     private val localDataSource: TimeoDataSource
 ) : TimeoRepository {
 
-    private val isUserSignedIn get() = AuthRepository.isUserSignedIn
-
-    override suspend fun addActivity(activity: Activity) {
-
-        if (isUserSignedIn) {
-
-            remoteDataSource.addActivity(activity)
-        } else {
-
-            localDataSource.addActivity(activity)
-        }
-    }
-
-    override suspend fun saveActivity(activity: Activity) {
-
-        if (isUserSignedIn) {
-
-            remoteDataSource.saveActivity(activity)
-        } else {
-
-            localDataSource.saveActivity(activity)
-        }
-    }
-
-    override suspend fun deleteActivity(activity: Activity) {
-
-        if (isUserSignedIn) {
-
-            remoteDataSource.deleteActivity(activity)
-        } else {
-
-            localDataSource.deleteActivity(activity)
-        }
-    }
-
-    override suspend fun addRecord(record: Record) {
-
-        if (isUserSignedIn) {
-
-            remoteDataSource.addRecord(record)
-        } else {
-
-            localDataSource.addRecord(record)
-        }
-    }
-
-    override suspend fun deleteRecord(record: Record) {
-
-        if (isUserSignedIn) {
-
-            remoteDataSource.deleteRecord(record)
-        } else {
-
-            localDataSource.deleteRecord(record)
-        }
-    }
-
     override val activitiesLiveData: LiveData<*>?
         get() = if (isUserSignedIn) mActivitiesLiveData else activities
 
     override val recordsLiveData: LiveData<*>?
         get() = if (isUserSignedIn) mRecordsLiveData else records
 
+    private val isUserSignedIn
+        get() = AuthRepository.isUserSignedIn
+
+    private val currentDataSource
+        get() = if (isUserSignedIn) remoteDataSource else localDataSource
+
     private val activities = localDataSource.activities
     private val records = localDataSource.records
     private val mActivitiesLiveData get() = remoteDataSource.activitiesLiveData
     private val mRecordsLiveData get() = remoteDataSource.recordsLiveData
+
+    override suspend fun addActivity(activity: Activity) {
+
+        currentDataSource.addActivity(activity)
+    }
+
+    override suspend fun saveActivity(activity: Activity) {
+
+        currentDataSource.saveActivity(activity)
+    }
+
+    override suspend fun deleteActivity(activity: Activity) {
+
+        currentDataSource.deleteActivity(activity)
+    }
+
+    override suspend fun addRecord(record: Record) {
+
+        currentDataSource.addRecord(record)
+    }
+
+    override suspend fun deleteRecord(record: Record) {
+
+        currentDataSource.deleteRecord(record)
+    }
 }

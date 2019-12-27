05ec7b00fb43f49c770dcd539869590e1cb48e64
Maksym Poliakov <maxpoliakov.git@gmail.com>
2019-12-27 14:25:25 +0200

    Document the FakeUserManager

--- app/src/test/java/com/jdevs/timeo/data/source/FakeUserManager.kt
+++ app/src/test/java/com/jdevs/timeo/data/source/FakeUserManager.kt
@@ -1,15 +1,27 @@
 package com.jdevs.timeo.data.source
 
+/**
+ * Fakes the Firebase Authentication user manager
+ */
 object FakeUserManager : UserManager {
 
+    /**
+     * Indicates whether the user is signed in
+     */
     override var isUserSignedIn = true
         private set
 
+    /**
+     * Fakes signed in state
+     */
     fun signIn() {
 
         isUserSignedIn = true
     }
 
+    /**
+     * Fakes not signed in state
+     */
     fun signOut() {
 
         isUserSignedIn = false

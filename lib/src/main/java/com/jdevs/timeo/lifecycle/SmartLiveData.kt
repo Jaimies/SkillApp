29317be1659060ba33e9bef98469a0e94327cad8
Maksym Poliakov <maxpoliakov.git@gmail.com>
2020-03-07 11:41:33 +0200

    Fix the issue with property access syntax and remove unneeded static constructors

--- lib/src/main/java/com/jdevs/timeo/lifecycle/SmartLiveData.kt
+++ lib/src/main/java/com/jdevs/timeo/lifecycle/SmartLiveData.kt
@@ -7,28 +7,9 @@ import androidx.lifecycle.MutableLiveData
  *
  * @param T type of data hold by this instance
  */
-class SmartLiveData<T> : MutableLiveData<T> {
+class SmartLiveData<T> : MutableLiveData<T>() {
 
-    private constructor() : super()
-    private constructor(value: T) : super(value)
-
-    override fun setValue(value: T) {
+    override fun setValue(value: T?) {
         if (this.value != value) super.setValue(value)
-    }
-
-    /**
-     * Static constructors that expose new instances as [MutableLiveData]
-     */
-    companion object {
-
-        /**
-         * Creates a SmartLiveData with no value assigned to it
-         */
-        operator fun <T> invoke(): MutableLiveData<T> = SmartLiveData()
-
-        /**
-         * Creates a SmartLiveData initialized with the given [value]
-         */
-        operator fun <T> invoke(value: T): MutableLiveData<T> = SmartLiveData(value)
     }
 }

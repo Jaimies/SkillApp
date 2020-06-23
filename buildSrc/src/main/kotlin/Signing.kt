d51a5ef41c2bcf777062fe72aa4dd95f0a997d2c
Maksym Poliakov <maxpoliakov.git@gmail.com>
2020-06-23 21:06:03 +0300

    chore(signingConfig): use relative path instead of absolute one

--- buildSrc/src/main/kotlin/Signing.kt
+++ buildSrc/src/main/kotlin/Signing.kt
@@ -1,6 +1,6 @@
 @Suppress("unused")
 object Signing {
-    const val keyPath = "/home/alex/StudioProjects/Timeo/signing.jks"
+    const val keyPath = "/path/to/keystore.jks"
     const val keyPassword = "PASSWORD"
     const val keyAlias = "KEY_ALIAS"
     const val storePassword = "PASSWORD"

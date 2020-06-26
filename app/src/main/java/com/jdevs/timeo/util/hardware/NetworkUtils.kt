1794994a1a6506f390823caab88ad9e2d4b13573
Maksym Poliakov <maxpoliakov.git@gmail.com>
2020-06-26 20:30:00 +0300

    refactor: remove Context.hasNetworkConnection extension val

--- app/src/main/java/com/jdevs/timeo/util/hardware/NetworkUtils.kt
+++ app/src/main/java/com/jdevs/timeo/util/hardware/NetworkUtils.kt
@@ -5,16 +5,6 @@ import android.net.ConnectivityManager
 import androidx.core.content.getSystemService
 import javax.inject.Inject
 
-@Suppress("DEPRECATION")
-val Context.hasNetworkConnection: Boolean
-    get() {
-        val connectivityManager = getSystemService<ConnectivityManager>()
-        val networkInfo = connectivityManager?.activeNetworkInfo
-
-        return networkInfo != null && networkInfo.isConnectedOrConnecting
-    }
-
-
 class NetworkUtils @Inject constructor(context: Context) {
     private val connectivityManager = context.getSystemService<ConnectivityManager>()
 

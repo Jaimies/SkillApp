d9f8ee399183232901923eb2c9b9b48234b6cffb
Maksym Poliakov <maxpoliakov.git@gmail.com>
2020-06-30 20:37:59 +0300

    refactor: upgrade minSdk to 21

--- buildSrc/src/main/kotlin/App.kt
+++ buildSrc/src/main/kotlin/App.kt
@@ -2,7 +2,7 @@
 object App {
     const val compileSdk = 29
     const val targetSdk = 29
-    const val minSdk = 19
+    const val minSdk = 21
     const val buildTools = "29.0.3"
     const val versionCode = 1
     const val versionName = "0.0.1-beta01"

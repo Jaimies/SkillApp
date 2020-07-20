349b5d45d86124eaee66bc1606ef2fe98be99037
Maksym Poliakov <maxpoliakov.git@gmail.com>
2020-07-20 13:52:21 +0300

    fix: incorrect ordering in history list

--- app/src/main/java/com/jdevs/timeo/ui/history/FirestoreHistoryAdapter.kt
+++ app/src/main/java/com/jdevs/timeo/ui/history/FirestoreHistoryAdapter.kt
@@ -2,9 +2,9 @@ package com.jdevs.timeo.ui.history
 
 import com.jdevs.timeo.model.RecordItem
 import com.jdevs.timeo.model.ViewItem
-import com.jdevs.timeo.shared.util.isDateAfter
 import com.jdevs.timeo.ui.common.adapter.DelegateAdapter
 import com.jdevs.timeo.ui.common.adapter.FirestoreListAdapter
+import org.threeten.bp.LocalDate
 
 class FirestoreHistoryAdapter(delegateAdapter: DelegateAdapter) :
     FirestoreListAdapter(delegateAdapter) {
@@ -13,43 +13,53 @@ class FirestoreHistoryAdapter(delegateAdapter: DelegateAdapter) :
         delegateAdapters.put(DATE_LABEL, DateLabelDelegateAdapter())
     }
 
-    override fun addItem(item: ViewItem) {
-
+    override fun addItem(item: ViewItem, index: Int) {
         item as RecordItem
-        val lastItem = items.lastOrNull { it is RecordItem } as RecordItem?
+        val creationDate = item.creationDate.toLocalDate()
 
-        if (dataItemCount == 0 || lastItem?.creationDate?.isDateAfter(item.creationDate) == true) {
+        addDateLabelIfNeeded(creationDate, index)
 
-            items += DateLabel(item.creationDate.toLocalDate())
-            notifyItemInserted(items.lastIndex)
-        }
+        val labelCount = items.count { it is DateLabel && it.date >= creationDate }
 
-        super.addItem(item)
+        super.addItem(item, labelCount + index)
+    }
+
+    override fun modifyItem(item: ViewItem, newIndex: Int) {
+        item as RecordItem
+        val count = items.count { it is DateLabel && it.date >= item.creationDate.toLocalDate() }
+        super.modifyItem(item, newIndex + count)
     }
 
     override fun removeItem(item: ViewItem) {
-
+        super.removeItem(item)
         item as RecordItem
+        removeDateLabelIfNeeded(item.creationDate.toLocalDate())
+    }
 
-        val countOfItems = items.count {
-            it is RecordItem && it.creationDate.toLocalDate() == item.creationDate.toLocalDate()
+    private fun addDateLabelIfNeeded(date: LocalDate, index: Int) {
+        val label = items.find { it is DateLabel && it.date == date }
+
+        if (label == null) {
+            val recordCount = items.count { it is DateLabel && it.date >= date }
+            items.add(recordCount + index, DateLabel(date))
+            notifyItemInserted(recordCount + index)
+        }
+    }
+
+    private fun removeDateLabelIfNeeded(date: LocalDate) {
+        val itemCount = items.count {
+            it is RecordItem && it.creationDate.toLocalDate() == date
         }
 
-        if (countOfItems == 1) {
-
-            val labelIndex = items.indexOfFirst {
-                it is DateLabel && it.date == item.creationDate.toLocalDate()
-            }
+        if (itemCount == 0) {
+            val labelIndex = items.indexOfFirst { it is DateLabel && it.date == date }
 
             items.removeAt(labelIndex)
             notifyItemRemoved(labelIndex)
         }
-
-        super.removeItem(item)
     }
 
     override fun getItemViewType(position: Int): Int {
-
         if (items[position] is DateLabel) return DATE_LABEL
         return super.getItemViewType(position)
     }

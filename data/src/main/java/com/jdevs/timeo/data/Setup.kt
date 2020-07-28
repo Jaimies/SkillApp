6a58a05c7505c50277f4ecc2f0574a314c18bba3
Maksym Poliakov <maxpoliakov.git@gmail.com>
2020-07-28 11:58:39 +0300

    refactor: split setupFirebase function into smaller functions

--- data/src/main/java/com/jdevs/timeo/data/Setup.kt
+++ data/src/main/java/com/jdevs/timeo/data/Setup.kt
@@ -6,9 +6,20 @@ import com.google.firebase.functions.ktx.functions
 import com.google.firebase.ktx.Firebase
 
 fun setupFirebase() {
-    if (!BuildConfig.DEBUG) return
+    if (BuildConfig.DEBUG)
+        setupEmulators()
+}
 
+private fun setupEmulators() {
+    setupFunctionsToUseEmulator()
+    setupFirestoreToUseEmulator()
+}
+
+private fun setupFunctionsToUseEmulator() {
     Firebase.functions("europe-west1").useFunctionsEmulator("10.0.2.2:5001")
+}
+
+private fun setupFirestoreToUseEmulator() {
     Firebase.firestore.firestoreSettings = firestoreSettings {
         host = "10.0.2.2:8080"
         isPersistenceEnabled = false

14d8e8e3f17da746d64a078d3eb5a853487cd428
Maksym Poliakov <maxpoliakov.git@gmail.com>
2020-08-03 19:44:18 +0300

    refactor: simplify TimePicker.kt

--- app/src/main/java/com/jdevs/timeo/util/fragment/TimePicker.kt
+++ app/src/main/java/com/jdevs/timeo/util/fragment/TimePicker.kt
@@ -4,14 +4,9 @@ import androidx.fragment.app.Fragment
 import com.wdullaer.materialdatetimepicker.time.TimePickerDialog
 
 inline fun Fragment.showTimePicker(crossinline onTimeSet: (hours: Int, minutes: Int) -> Unit) {
+    val dialog = TimePickerDialog.newInstance({ _, hour, minute, _ ->
+        if (hour != 0 || minute != 0) onTimeSet(hour, minute)
+    }, 0, 0, true)
 
-    val onTimeSetListener = { _: TimePickerDialog, hour: Int, minute: Int, _: Int ->
-
-        if (hour != 0 || minute != 0) {
-            onTimeSet(hour, minute)
-        }
-    }
-
-    val dialog = TimePickerDialog.newInstance(onTimeSetListener, 0, 0, true)
     dialog.show(childFragmentManager, null)
 }

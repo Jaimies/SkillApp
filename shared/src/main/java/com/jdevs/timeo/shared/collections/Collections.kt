d9a262f75c05c09889faee254fabfde40d762e32
Maksym Poliakov <maxpoliakov.git@gmail.com>
2020-08-04 11:25:58 +0300

    style: formatting in :shared module

--- shared/src/main/java/com/jdevs/timeo/shared/collections/Collections.kt
+++ shared/src/main/java/com/jdevs/timeo/shared/collections/Collections.kt
@@ -1,11 +1,11 @@
 package com.jdevs.timeo.shared.collections
 
-import kotlin.contracts.InvocationKind
+import kotlin.contracts.InvocationKind.EXACTLY_ONCE
 import kotlin.contracts.contract
 
 inline fun <T> MutableList<T>.update(index: Int, transform: (T) -> T) {
     contract {
-        callsInPlace(transform, InvocationKind.EXACTLY_ONCE)
+        callsInPlace(transform, EXACTLY_ONCE)
     }
     require(index >= 0) { "Index must greater than or equal to 0, got $index" }
     this[index] = transform(this[index])
--- shared/src/main/java/com/jdevs/timeo/shared/util/LiveData.kt
+++ shared/src/main/java/com/jdevs/timeo/shared/util/LiveData.kt
@@ -4,6 +4,5 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.map
 
 inline fun <X, Y> LiveData<List<X>>.mapList(crossinline transform: (X) -> Y): LiveData<List<Y>> {
-
     return map { list -> list.map(transform) }
 }

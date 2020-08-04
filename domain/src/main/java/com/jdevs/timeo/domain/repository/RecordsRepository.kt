b76eb6dc81f7f9ecb2862852099989058345e0bf
Maksym Poliakov <maxpoliakov.git@gmail.com>
2020-08-04 13:10:19 +0300

    fix: rename method

--- domain/src/main/java/com/jdevs/timeo/domain/repository/RecordsRepository.kt
+++ domain/src/main/java/com/jdevs/timeo/domain/repository/RecordsRepository.kt
@@ -6,11 +6,9 @@ import com.jdevs.timeo.domain.model.Operation
 import com.jdevs.timeo.domain.model.Record
 
 interface RecordsRepository {
-
     val records: DataSource.Factory<Int, Record>
-    fun getRecordsRemote(fetchNewItems: Boolean): List<LiveData<Operation<Record>>>
 
+    fun getRecordsFromRemote(fetchNewItems: Boolean): List<LiveData<Operation<Record>>>
     suspend fun addRecord(record: Record)
-
     suspend fun deleteRecord(record: Record)
 }

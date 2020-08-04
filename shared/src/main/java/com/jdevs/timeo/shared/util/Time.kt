156e8ab9bf30da29e080487d0f502b043e538590
Maksym Poliakov <maxpoliakov.git@gmail.com>
2020-08-04 16:47:57 +0300

    refactor: Time.kt

--- shared/src/main/java/com/jdevs/timeo/shared/util/Time.kt
+++ shared/src/main/java/com/jdevs/timeo/shared/util/Time.kt
@@ -4,11 +4,11 @@ import org.threeten.bp.DateTimeUtils.toDate
 import org.threeten.bp.DateTimeUtils.toInstant
 import org.threeten.bp.DayOfWeek
 import org.threeten.bp.Instant
-import org.threeten.bp.LocalDate
 import org.threeten.bp.Month
 import org.threeten.bp.OffsetDateTime
 import org.threeten.bp.ZoneOffset
 import org.threeten.bp.format.TextStyle
+import org.threeten.bp.temporal.ChronoUnit
 import org.threeten.bp.temporal.ChronoUnit.DAYS
 import org.threeten.bp.temporal.ChronoUnit.MONTHS
 import org.threeten.bp.temporal.ChronoUnit.WEEKS
@@ -28,9 +28,18 @@ fun Date.toOffsetDate(): OffsetDateTime = toInstant(this).atOffset(currentOffset
 fun OffsetDateTime.toDate(): Date = toDate(this.toInstant())
 
 val OffsetDateTime.daysAgo get() = DAYS.between(this, OffsetDateTime.now())
-val OffsetDateTime.daysSinceEpoch get() = LocalDate.from(this).toEpochDay().toInt()
-val OffsetDateTime.weeksSinceEpoch get() = WEEKS.between(EPOCH, this).toInt()
-val OffsetDateTime.monthSinceEpoch get() = MONTHS.between(EPOCH, this).toInt()
+
+fun getUnitsSinceEpoch(unit: ChronoUnit): Int {
+    return OffsetDateTime.now().getUnitsSinceEpoch(unit)
+}
+
+fun OffsetDateTime.getUnitsSinceEpoch(unit: ChronoUnit): Int {
+    return unit.between(EPOCH, this).toInt()
+}
+
+val OffsetDateTime.daysSinceEpoch get() = getUnitsSinceEpoch(DAYS)
+val OffsetDateTime.weeksSinceEpoch get() = getUnitsSinceEpoch(WEEKS)
+val OffsetDateTime.monthSinceEpoch get() = getUnitsSinceEpoch(MONTHS)
 
 const val WEEK_DAYS = 7
 const val HOUR_MINUTES = 60

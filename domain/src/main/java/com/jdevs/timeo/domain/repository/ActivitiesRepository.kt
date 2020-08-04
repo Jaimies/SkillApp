813764a6b1bf8ddcf6b25ee7f8bc223252763d24
Maksym Poliakov <maxpoliakov.git@gmail.com>
2020-08-04 13:07:46 +0300

    style: formatting in ActivitiesRepository

--- domain/src/main/java/com/jdevs/timeo/domain/repository/ActivitiesRepository.kt
+++ domain/src/main/java/com/jdevs/timeo/domain/repository/ActivitiesRepository.kt
@@ -6,18 +6,14 @@ import com.jdevs.timeo.domain.model.Activity
 import com.jdevs.timeo.domain.model.Operation
 
 interface ActivitiesRepository {
-
     val activities: DataSource.Factory<Int, Activity>
-    fun getRemoteActivities(fetchNewItems: Boolean): List<LiveData<Operation<Activity>>>
-    fun getTopLevelActivitiesFromCache(activityIdToExclude: String): LiveData<List<Activity>>
-
     val topActivities: LiveData<List<Activity>>
 
+    fun getRemoteActivities(fetchNewItems: Boolean): List<LiveData<Operation<Activity>>>
+    fun getTopLevelActivitiesFromCache(activityIdToExclude: String): LiveData<List<Activity>>
     fun getActivityById(id: String): LiveData<Activity>
 
     suspend fun addActivity(activity: Activity)
-
     suspend fun saveActivity(activity: Activity)
-
     suspend fun deleteActivity(activity: Activity)
 }

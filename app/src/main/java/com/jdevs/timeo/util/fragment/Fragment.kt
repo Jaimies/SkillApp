6d61d870f72791d1e959f3c65ba25aff7c0a32c0
Maksym Poliakov <maxpoliakov.git@gmail.com>
2020-08-04 13:11:48 +0300

    refactor: Fragment.kt

--- app/src/main/java/com/jdevs/timeo/util/fragment/Fragment.kt
+++ app/src/main/java/com/jdevs/timeo/util/fragment/Fragment.kt
@@ -7,24 +7,27 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.observe
 import androidx.navigation.fragment.findNavController
 import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.snackbar.Snackbar.LENGTH_LONG
 import com.jdevs.timeo.ui.MainActivity
 
 fun Fragment.snackbar(@StringRes resId: Int) =
-    Snackbar.make(requireView(), resId, Snackbar.LENGTH_LONG).show()
+    Snackbar.make(requireView(), resId, LENGTH_LONG).show()
 
 fun Fragment.navigateToGraph(@IdRes graphId: Int) {
 
     mainActivity.navigateToGraph(graphId)
 
     findNavController().run {
-
         popBackStack(graph.id, true)
     }
 }
 
-inline fun <T> Fragment.observe(liveData: LiveData<T>, crossinline onEvent: (T) -> Unit) {
+fun Fragment.navigate(@IdRes resId: Int) {
+    findNavController().navigate(resId)
+}
 
-    liveData.observe(viewLifecycleOwner) { onEvent(it) }
+inline fun <T> Fragment.observe(liveData: LiveData<T>, crossinline onChange: (T) -> Unit) {
+    liveData.observe(viewLifecycleOwner) { onChange(it) }
 }
 
 val Fragment.mainActivity get() = requireActivity() as MainActivity

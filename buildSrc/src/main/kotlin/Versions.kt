e3702de98d51c0d10f6e70f5051b9cb9baa99b25
Maksym Poliakov <maxpoliakov.git@gmail.com>
2020-09-01 19:12:58 +0300

    fix: tests failing because of an internal error

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -32,6 +32,8 @@ object Versions {
     const val gradle = "4.0.0"
     const val gms = "4.3.3"
 
-    const val hamcrest = "1.3"
+    const val kotestJUnit5 = "4.2.2"
+    const val kotestConsole = "4.1.2"
+
     const val debugDb = "1.0.6"
 }

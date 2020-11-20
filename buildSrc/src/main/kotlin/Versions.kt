f615ee54c8b6a25eddf538d9327964e17a4ea393
Maksym Poliakov <maxpoliakov.git@gmail.com>
2020-11-20 21:39:17 +0200

    chore: upgrade deps

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,21 +1,21 @@
 @Suppress("unused")
 object Versions {
-    const val kotlin = "1.4.0"
-    const val coroutines = "1.3.4"
+    const val kotlin = "1.4.20"
+    const val coroutines = "1.4.1"
 
-    const val androidxCore = "1.3.0"
-    const val appcompat = "1.1.0"
-    const val constraintLayout = "1.1.3"
+    const val androidxCore = "1.3.2"
+    const val appcompat = "1.2.0"
+    const val constraintLayout = "2.0.4"
     const val legacy = "1.0.0"
 
     const val room = "2.2.5"
     const val paging = "2.1.2"
-    const val navigation = "2.3.0"
+    const val navigation = "2.3.1"
     const val lifecycle = "2.2.0"
     const val livedata = "2.2.0"
     const val fragment = "1.2.5"
 
-    const val material = "1.3.0-alpha01"
+    const val material = "1.3.0-alpha03"
     const val MPAndroidChart = "3.1.0"
     const val datepicker = "4.2.3"
     const val viewpager = "1.0.0"
@@ -24,7 +24,7 @@ object Versions {
     const val threeTenBP = "1.4.0"
 
     const val hilt = "2.28-alpha"
-    const val androidxHilt = "1.0.0-alpha01"
+    const val androidxHilt = "1.0.0-alpha02"
 
     const val lint = "26.5.3"
     const val dataBinding = "3.1.4"

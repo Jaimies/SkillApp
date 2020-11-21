64699548dfea645afa43d0f0ec7fe92b4387b08a
Maksym Poliakov <maxpoliakov.git@gmail.com>
2020-11-21 10:48:14 +0200

    fix: trying to act on Flow as on DataSource.Factory

--- app/src/main/java/com/jdevs/timeo/ui/activities/ActivitiesViewModel.kt
+++ app/src/main/java/com/jdevs/timeo/ui/activities/ActivitiesViewModel.kt
@@ -1,27 +1,25 @@
 package com.jdevs.timeo.ui.activities
 
 import androidx.hilt.lifecycle.ViewModelInject
-import androidx.paging.toLiveData
 import androidx.lifecycle.ViewModel
-import com.jdevs.timeo.domain.model.Activity
+import androidx.lifecycle.asLiveData
 import com.jdevs.timeo.domain.model.Record
 import com.jdevs.timeo.domain.usecase.activities.GetActivitiesUseCase
 import com.jdevs.timeo.domain.usecase.records.AddRecordUseCase
 import com.jdevs.timeo.lifecycle.SingleLiveEvent
 import com.jdevs.timeo.model.ActivityItem
 import com.jdevs.timeo.model.mapToPresentation
+import com.jdevs.timeo.shared.util.mapList
 import com.jdevs.timeo.util.lifecycle.launchCoroutine
-
-const val ACTIVITIES_PAGE_SIZE = 20
 
 class ActivitiesViewModel @ViewModelInject constructor(
     getActivities: GetActivitiesUseCase,
     private val addRecord: AddRecordUseCase
 ) : ViewModel() {
 
-    val activities =
-        getActivities.run().map(Activity::mapToPresentation)
-            .toLiveData(ACTIVITIES_PAGE_SIZE)
+    val activities = getActivities.run()
+        .mapList { it.mapToPresentation() }
+        .asLiveData()
 
     val navigateToAddEdit = SingleLiveEvent<Any>()
 

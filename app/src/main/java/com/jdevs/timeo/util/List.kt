0efd05863de0e5b102aa71c644e5fbe01d5fe606
Maksym Poliakov <maxpoliakov.git@gmail.com>
2020-11-22 14:10:51 +0200

    test: cover List.kt

--- app/src/main/java/com/jdevs/timeo/util/List.kt
+++ app/src/main/java/com/jdevs/timeo/util/List.kt
@@ -2,4 +2,4 @@
 
 package com.jdevs.timeo.util
 
-fun <T> MutableList<T>?.unshift(element: T) = this?.apply { add(0, element) }
+fun <T> List<T>?.unshift(element: T) = this?.run { listOf(element) + this }

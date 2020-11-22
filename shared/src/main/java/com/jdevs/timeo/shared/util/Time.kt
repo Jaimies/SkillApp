c2243378f20b62f3bfedef15bb1805dd3e96cea6
Maksym Poliakov <maxpoliakov.git@gmail.com>
2020-11-22 18:54:41 +0200

    refactor(time util): allow using some getters on more generic types

--- shared/src/main/java/com/jdevs/timeo/shared/util/Time.kt
+++ shared/src/main/java/com/jdevs/timeo/shared/util/Time.kt
@@ -1,19 +1,17 @@
 package com.jdevs.timeo.shared.util
 
 import java.time.DayOfWeek
-import java.time.Instant
+import java.time.LocalDate
 import java.time.Month
 import java.time.OffsetDateTime
-import java.time.ZoneOffset
 import java.time.format.TextStyle
-import java.time.temporal.ChronoUnit
 import java.time.temporal.ChronoUnit.DAYS
-import java.time.temporal.ChronoUnit.MONTHS
 import java.time.temporal.ChronoUnit.WEEKS
+import java.time.temporal.ChronoUnit.MONTHS
+import java.time.temporal.Temporal
 import java.util.Locale
 
-val currentOffset: ZoneOffset get() = OffsetDateTime.now().offset
-val EPOCH: OffsetDateTime get() = Instant.EPOCH.atOffset(currentOffset)
+val EPOCH: LocalDate get() = LocalDate.ofEpochDay(0)
 
 val Month.shortName: String
     get() = getDisplayName(TextStyle.SHORT, Locale.getDefault())
@@ -21,7 +19,10 @@ val Month.shortName: String
 val DayOfWeek.shortName: String
     get() = getDisplayName(TextStyle.SHORT, Locale.getDefault())
 
-val OffsetDateTime.daysAgo get() = DAYS.between(this, OffsetDateTime.now())
+val Temporal.daysAgo get() = DAYS.between(this, getCurrentDateTime())
+val Temporal.daysSinceEpoch get() = DAYS.between(EPOCH, this)
+val Temporal.weeksSinceEpoch get() = WEEKS.between(EPOCH, this)
+val Temporal.monthSinceEpoch get() = MONTHS.between(EPOCH, this)
 
 fun getUnitsSinceEpoch(unit: ChronoUnit): Int {
     return OffsetDateTime.now().getUnitsSinceEpoch(unit)
@@ -30,9 +31,3 @@ fun getUnitsSinceEpoch(unit: ChronoUnit): Int {
 fun OffsetDateTime.getUnitsSinceEpoch(unit: ChronoUnit): Int {
     return unit.between(EPOCH, this).toInt()
 }
-
-val OffsetDateTime.daysSinceEpoch get() = getUnitsSinceEpoch(DAYS)
-val OffsetDateTime.weeksSinceEpoch get() = getUnitsSinceEpoch(WEEKS)
-val OffsetDateTime.monthSinceEpoch get() = getUnitsSinceEpoch(MONTHS)
-
-const val HOUR_MINUTES = 60

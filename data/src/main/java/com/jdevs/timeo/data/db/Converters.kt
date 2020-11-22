7b6ff335152532afa151601bb3920dc5e7a39a94
Maksym Poliakov <maxpoliakov.git@gmail.com>
2020-11-22 14:12:15 +0200

    refactor: make Converters a class

--- data/src/main/java/com/jdevs/timeo/data/db/Converters.kt
+++ data/src/main/java/com/jdevs/timeo/data/db/Converters.kt
@@ -4,16 +4,13 @@ import androidx.room.TypeConverter
 import java.time.OffsetDateTime
 import java.time.format.DateTimeFormatter
 
-object Converters {
-
+class Converters {
     private val formatter = DateTimeFormatter.ISO_OFFSET_DATE_TIME
 
-    @JvmStatic
     @TypeConverter
     fun toOffsetDateTime(value: String): OffsetDateTime =
         formatter.parse(value, OffsetDateTime::from)
 
-    @JvmStatic
     @TypeConverter
     fun fromOffsetDateTime(date: OffsetDateTime): String = date.format(formatter)
 }

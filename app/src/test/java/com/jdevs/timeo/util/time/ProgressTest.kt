f75a9fe6a1191a82f9cba0e78b4cdd10a90e2ade
Maksym Poliakov <maxpoliakov.git@gmail.com>
2020-11-22 17:51:29 +0200

    fix: ProgressTest not compiling

--- app/src/test/java/com/jdevs/timeo/util/time/ProgressTest.kt
+++ app/src/test/java/com/jdevs/timeo/util/time/ProgressTest.kt
@@ -3,11 +3,12 @@ package com.jdevs.timeo.util.time
 import com.jdevs.timeo.daysAgoDate
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.shouldBe
+import java.time.Duration
 
 class ProgressTest : StringSpec({
     "get avg week hours" {
-        getAvgWeekHours(238, daysAgoDate(7)) shouldBe "2"
-        getAvgWeekHours(444, daysAgoDate(8)) shouldBe "3.7"
+        getAvgWeekHours(Duration.ofMinutes(238), daysAgoDate(7)) shouldBe "2"
+        getAvgWeekHours(Duration.ofMinutes(444), daysAgoDate(8)) shouldBe "3.7"
     }
 
     "get days spent since" {

1fbee77fa3d9e62763c84a0480818bd5c7f9349f
Maksym Poliakov <maxpoliakov.git@gmail.com>
2020-11-27 18:57:05 +0200

    test: cover StatsDao

---
+++ data/src/androidTest/java/com/maxpoliakov/skilapp/data/stats/Flow.kt
@@ -0,0 +1,19 @@
+package com.maxpoliakov.skilapp.data.stats
+
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
+import kotlin.coroutines.resume
+import kotlin.coroutines.suspendCoroutine
+
+suspend fun <T> Flow<T>.await(): T {
+    return suspendCoroutine { continuation ->
+        CoroutineScope(Dispatchers.IO).launch {
+            this@await.collect { value ->
+                continuation.resume(value)
+            }
+        }
+    }
+}
---
+++ data/src/androidTest/java/com/maxpoliakov/skilapp/data/stats/StatsDaoTest.kt
@@ -0,0 +1,100 @@
+package com.maxpoliakov.skilapp.data.stats
+
+import androidx.room.Room
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
+import com.maxpoliakov.skillapp.data.activities.ActivitiesDao
+import com.maxpoliakov.skillapp.data.activities.DBActivity
+import com.maxpoliakov.skillapp.data.db.AppDatabase
+import com.maxpoliakov.skillapp.data.stats.DBStatistic
+import com.maxpoliakov.skillapp.data.stats.StatsDao
+import com.maxpoliakov.skillapp.shared.util.daysSinceEpoch
+import io.kotest.matchers.shouldBe
+import kotlinx.coroutines.runBlocking
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import java.time.Duration
+import java.time.LocalDate
+
+@RunWith(AndroidJUnit4::class)
+class StatsDaoTest {
+    private lateinit var db: AppDatabase
+    private lateinit var statsDao: StatsDao
+    private lateinit var activitiesDao: ActivitiesDao
+
+    @Before
+    fun beforeEach() = runBlocking {
+        val context = InstrumentationRegistry.getInstrumentation().targetContext
+        db = Room.inMemoryDatabaseBuilder(
+            context, AppDatabase::class.java
+        ).build()
+        statsDao = db.statsDao()
+        activitiesDao = db.activitiesDao()
+        activitiesDao.insert(DBActivity())
+    }
+
+    @Test
+    fun addRecord() = runBlocking {
+        statsDao.addRecord(activityId, day, recordTime)
+        statsDao.getStats(activityId).await() shouldBe listOf(
+            DBStatistic(day, activityId, Duration.ofMinutes(100))
+        )
+    }
+
+    @Test
+    fun addRecord_recordAtGivenDayExists_sumsTime() = runBlocking {
+        statsDao.addRecord(activityId, day, recordTime)
+        statsDao.addRecord(activityId, day, recordTime)
+        statsDao.getStats(activityId).await() shouldBe listOf(
+            DBStatistic(day, activityId, Duration.ofMinutes(200))
+        )
+    }
+
+    @Test
+    fun addRecord_multipleActivitiesAtOneDay_areHandledIndependently() = runBlocking {
+        activitiesDao.insert(DBActivity())
+        statsDao.addRecord(activityId, day, recordTime)
+        statsDao.addRecord(otherActivityId, day, recordTime)
+        statsDao.getStats(activityId).await() shouldBe listOf(
+            DBStatistic(day, activityId, Duration.ofMinutes(100))
+        )
+    }
+
+    @Test
+    fun getStats_selectsOnlyFromSpecifiedActivity() = runBlocking {
+        statsDao.addRecord(activityId, day, recordTime)
+        statsDao.getStats(otherActivityId).await() shouldBe listOf()
+    }
+
+    @Test
+    fun getStats_selectsOnlyWithPositiveTime() = runBlocking {
+        statsDao.addRecord(activityId, day, -recordTime)
+        statsDao.getStats(activityId).await() shouldBe listOf()
+    }
+
+    @Test
+    fun getStats_ignoresOlderThan13DaysAgo() = runBlocking {
+        statsDao.addRecord(activityId, day - 14, recordTime)
+        statsDao.getStats(activityId).await() shouldBe listOf()
+    }
+
+    @Test
+    fun getStats_ignoresMoreRecentThanToday() = runBlocking {
+        statsDao.addRecord(activityId, day + 1, recordTime)
+        statsDao.getStats(activityId).await() shouldBe listOf()
+    }
+
+    @After
+    fun afterEach() {
+        db.close()
+    }
+
+    companion object {
+        private val day = LocalDate.now().daysSinceEpoch
+        private const val activityId = 1
+        private const val otherActivityId = 2
+        private const val recordTime = 100L
+    }
+}

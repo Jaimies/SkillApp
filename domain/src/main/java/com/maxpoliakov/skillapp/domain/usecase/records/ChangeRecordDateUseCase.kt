c1f9341f1b63c71721e25b0909b948c943acce3d
Maksym Poliakov <maxpoliakov.git@gmail.com>
2020-12-04 11:11:17 +0200

    fix: changing record date does not decrease statistic time on the previous day

--- domain/src/main/java/com/maxpoliakov/skillapp/domain/usecase/records/ChangeRecordDateUseCase.kt
+++ domain/src/main/java/com/maxpoliakov/skillapp/domain/usecase/records/ChangeRecordDateUseCase.kt
@@ -1,20 +1,18 @@
 package com.maxpoliakov.skillapp.domain.usecase.records
 
 import com.maxpoliakov.skillapp.domain.repository.RecordsRepository
-import com.maxpoliakov.skillapp.domain.repository.StatsRepository
 import java.time.LocalDate
 import javax.inject.Inject
 
 class ChangeRecordDateUseCase @Inject constructor(
-    private val recordsRepository: RecordsRepository,
-    private val statsRepository: StatsRepository
+    private val addRecord: AddRecordUseCase,
+    private val deleteRecord: DeleteRecordUseCase,
+    private val recordsRepository: RecordsRepository
 ) {
 
     suspend fun run(id: Int, newDate: LocalDate) {
         val oldRecord = recordsRepository.getRecord(id)
-        val newRecord = oldRecord.copy(date = newDate)
-        recordsRepository.updateRecord(newRecord)
-        statsRepository.addRecord(oldRecord.copy(time = oldRecord.time.negated()))
-        statsRepository.addRecord(newRecord)
+        deleteRecord.run(id)
+        addRecord.run(oldRecord.copy(date = newDate))
     }
 }

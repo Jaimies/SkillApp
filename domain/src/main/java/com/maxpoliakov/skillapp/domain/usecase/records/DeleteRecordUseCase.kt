a4593668841f54afa70dbed454be460132ea96b2
Maksym Poliakov <maxpoliakov.git@gmail.com>
2020-12-04 11:09:01 +0200

    fix: deleting a record does not decrease activity time

--- domain/src/main/java/com/maxpoliakov/skillapp/domain/usecase/records/DeleteRecordUseCase.kt
+++ domain/src/main/java/com/maxpoliakov/skillapp/domain/usecase/records/DeleteRecordUseCase.kt
@@ -3,19 +3,22 @@ package com.maxpoliakov.skillapp.domain.usecase.records
 import com.maxpoliakov.skillapp.domain.model.Record
 import com.maxpoliakov.skillapp.domain.repository.SkillRepository
 import com.maxpoliakov.skillapp.domain.repository.RecordsRepository
+import com.maxpoliakov.skillapp.domain.repository.StatsRepository
 import kotlinx.coroutines.Dispatchers.IO
 import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class DeleteRecordUseCase @Inject constructor(
     private val recordsRepository: RecordsRepository,
-    private val skillRepository: SkillRepository
+    private val skillRepository: SkillRepository,
+    private val statsRepository: StatsRepository
 ) {
     suspend fun run(id: Int) {
         withContext(IO) {
             val record = recordsRepository.getRecord(id)
             skillRepository.decreaseTime(record.skillId, record.time)
             recordsRepository.deleteRecord(record)
+            statsRepository.addRecord(record.copy(time = record.time.negated()))
         }
     }
 }

c2becdb1978f774302b54738d3b981b5c605e6e7
Maksym Poliakov <maxpoliakov.git@gmail.com>
2020-12-05 20:49:00 +0200

    refactor: remove unused test utils

--- sharedTest/src/main/java/com/maxpoliakov/skillapp/test/DataSourceFactory.kt
+++
@@ -1,8 +0,0 @@
-package com.maxpoliakov.skillapp.test
-
-import androidx.paging.DataSource
-import androidx.paging.toLiveData
-
-suspend fun <T> DataSource.Factory<*, T>.getValue(): List<T> {
-    return this.toLiveData(Int.MAX_VALUE).awaitValue()
-}

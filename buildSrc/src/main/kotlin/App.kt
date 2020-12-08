515d5f4c27360677a606798175e5a73a4f58b6df
Maksym Poliakov <maxpoliakov.git@gmail.com>
2020-12-08 22:56:29 +0200

    chore: change the app version to "1.0.0"

--- buildSrc/src/main/kotlin/App.kt
+++ buildSrc/src/main/kotlin/App.kt
@@ -5,6 +5,6 @@ object App {
     const val minSdk = 21
     const val buildTools = "30.0.2"
     const val versionCode = 1
-    const val versionName = "0.0.1-beta01"
+    const val versionName = "1.0.0"
     const val applicationId = "com.maxpoliakov.skillapp"
 }

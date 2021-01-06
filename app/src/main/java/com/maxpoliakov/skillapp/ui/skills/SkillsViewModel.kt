030b2a581bedf1343a2464fa734fde1154d24749
Maksym Poliakov <maxpoliakov.git@gmail.com>
2021-01-06 09:38:42 +0200

    feat: add a stopwatch banner in the skills fragment

--- app/src/main/java/com/maxpoliakov/skillapp/ui/skills/SkillsViewModel.kt
+++ app/src/main/java/com/maxpoliakov/skillapp/ui/skills/SkillsViewModel.kt
@@ -4,13 +4,22 @@ import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
 import androidx.lifecycle.map
+import com.maxpoliakov.skillapp.domain.usecase.skill.GetSkillByIdUseCase
 import com.maxpoliakov.skillapp.domain.usecase.skill.GetSkillsUseCase
 import com.maxpoliakov.skillapp.model.mapToPresentation
+import com.maxpoliakov.skillapp.shared.util.getZonedDateTime
 import com.maxpoliakov.skillapp.shared.util.mapList
 import com.maxpoliakov.skillapp.util.lifecycle.SingleLiveEvent
+import com.maxpoliakov.skillapp.util.stopwatch.StopwatchState.Running
+import com.maxpoliakov.skillapp.util.stopwatch.StopwatchUtil
+import kotlinx.coroutines.flow.flatMapLatest
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.flow.map
 
 class SkillsViewModel @ViewModelInject constructor(
-    getSkills: GetSkillsUseCase
+    getSkill: GetSkillByIdUseCase,
+    getSkills: GetSkillsUseCase,
+    stopwatchUtil: StopwatchUtil
 ) : ViewModel() {
 
     val skills = getSkills.run()
@@ -18,6 +27,16 @@ class SkillsViewModel @ViewModelInject constructor(
         .asLiveData()
 
     val isEmpty = skills.map { it.isEmpty() }
+
+    val trackingSkill = stopwatchUtil.state.flatMapLatest { state ->
+        if (state is Running) getSkill.run(state.skillId)
+        else flowOf(null)
+    }.asLiveData()
+
+    val stopwatchStartTime = stopwatchUtil.state.map { state ->
+        if (state is Running) state.startTime else getZonedDateTime()
+    }.asLiveData()
+    val isActive = stopwatchUtil.state.map { it is Running }.asLiveData()
 
     val navigateToAddEdit = SingleLiveEvent<Any>()
     fun navigateToAddSkill() = navigateToAddEdit.call()

72320194e1652b0c9c0881f50e8d9c4bd9b27140
Maksym Poliakov <maxpoliakov.git@gmail.com>
2021-07-06 21:29:55 +0300

    ui: add icons in settings

--- app/src/main/java/com/maxpoliakov/skillapp/ui/settings/SettingsFragment.kt
+++ app/src/main/java/com/maxpoliakov/skillapp/ui/settings/SettingsFragment.kt
@@ -1,10 +1,12 @@
 package com.maxpoliakov.skillapp.ui.settings
 
 import android.os.Bundle
+import android.view.View
 import androidx.preference.ListPreference
 import androidx.preference.PreferenceFragmentCompat
 import com.maxpoliakov.skillapp.R
 import com.maxpoliakov.skillapp.model.Theme
+import com.maxpoliakov.skillapp.util.ui.dp
 import com.maxpoliakov.skillapp.util.ui.setTheme
 
 class SettingsFragment : PreferenceFragmentCompat() {
@@ -16,5 +18,10 @@ class SettingsFragment : PreferenceFragmentCompat() {
             setTheme(Theme.valueOf(newValue as String))
             true
         }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        listView.setPadding(12.dp.toPx(requireContext()), 0, 0, 0)
     }
 }

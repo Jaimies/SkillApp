26e91ae3173625b0ffbe8a9e4e1a53b2a5119b1a
Maksym Poliakov <maxpoliakov.git@gmail.com>
2021-08-08 20:15:15 +0300

    fix: not navigating to detail of the currently tracked skill when stopwatch banner clicked

--- app/src/main/java/com/maxpoliakov/skillapp/ui/skills/StopwatchViewModel.kt
+++ app/src/main/java/com/maxpoliakov/skillapp/ui/skills/StopwatchViewModel.kt
@@ -3,18 +3,18 @@ package com.maxpoliakov.skillapp.ui.skills
 import androidx.lifecycle.asLiveData
 import com.maxpoliakov.skillapp.domain.usecase.skill.GetSkillByIdUseCase
 import com.maxpoliakov.skillapp.shared.util.getZonedDateTime
+import com.maxpoliakov.skillapp.util.navigation.NavigationUtil
 import com.maxpoliakov.skillapp.util.stopwatch.StopwatchState
 import com.maxpoliakov.skillapp.util.stopwatch.StopwatchUtil
 import kotlinx.coroutines.flow.flatMapLatest
 import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.flow.map
 import javax.inject.Inject
-import javax.inject.Singleton
 
-@Singleton
 class StopwatchViewModel @Inject constructor(
     private val stopwatchUtil: StopwatchUtil,
     private val getSkill: GetSkillByIdUseCase,
+    private val navigationUtil: NavigationUtil,
 ) {
     val trackingSkill = stopwatchUtil.state.flatMapLatest { state ->
         if (state is StopwatchState.Running) getSkill.run(state.skillId)
@@ -29,5 +29,7 @@ class StopwatchViewModel @Inject constructor(
 
     fun stopTimer() = stopwatchUtil.stop()
 
-    fun navigateToCurrentlyTrackedSkill() {}
+    fun navigateToCurrentlyTrackedSkill() {
+        navigationUtil.navigateToSkillDetail(trackingSkill.value!!.id)
+    }
 }

ddd81a5bf1b9876a9c13bab34d1ea47b015bb662
Maksym Poliakov <maxpoliakov.git@gmail.com>
2021-08-11 21:10:17 +0300

    fix: support diffing SkillGroups in DiffCallback

--- app/src/main/java/com/maxpoliakov/skillapp/ui/skills/SkillDiffCallback.kt
+++ app/src/main/java/com/maxpoliakov/skillapp/ui/skills/SkillDiffCallback.kt
@@ -2,25 +2,19 @@ package com.maxpoliakov.skillapp.ui.skills
 
 import androidx.recyclerview.widget.DiffUtil
 import com.maxpoliakov.skillapp.domain.model.Skill
+import com.maxpoliakov.skillapp.domain.model.SkillGroup
+import com.maxpoliakov.skillapp.ui.skills.stopwatch.StopwatchUiModel
 
 class SkillDiffCallback : DiffUtil.ItemCallback<Any>() {
     override fun areItemsTheSame(oldItem: Any, newItem: Any): Boolean {
-        return compare(oldItem, newItem) { old, new -> old.id == new.id }
+        return oldItem is StopwatchUiModel && newItem is StopwatchUiModel
+                || oldItem is Skill && newItem is Skill && oldItem.id == newItem.id
+                || oldItem is SkillGroup && newItem is SkillGroup && newItem.id == oldItem.id
     }
 
     override fun areContentsTheSame(oldItem: Any, newItem: Any): Boolean {
-        return compare(oldItem, newItem) { old, new ->
-            // order should be ignored when comparing for equality
-            old.copy(order = 0) == new.copy(order = 0)
-        }
-    }
-
-    private fun compare(
-        oldItem: Any,
-        newItem: Any,
-        predicate: (old: Skill, new: Skill) -> Boolean
-    ): Boolean {
-        return oldItem !is Skill && newItem !is Skill
-                || oldItem is Skill && newItem is Skill && predicate(oldItem, newItem)
+        return oldItem is StopwatchUiModel && newItem is StopwatchUiModel
+                || oldItem is Skill && newItem is Skill && oldItem.copy(order = 0) == newItem.copy(order = 0)
+                || oldItem is SkillGroup && newItem is SkillGroup && newItem == oldItem
     }
 }

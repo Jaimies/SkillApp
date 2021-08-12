76e2646424f54fee3739f875c8beb4aa04917661
Maksym Poliakov <maxpoliakov.git@gmail.com>
2021-08-12 09:02:11 +0300

    refactor(SkillDetailFragment): extract the logic for handling the collapsing toolbar

--- app/src/main/java/com/maxpoliakov/skillapp/ui/skilldetail/SkillDetailFragment.kt
+++ app/src/main/java/com/maxpoliakov/skillapp/ui/skilldetail/SkillDetailFragment.kt
@@ -5,36 +5,35 @@ import android.view.LayoutInflater
 import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
-import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.maxpoliakov.skillapp.MainDirections
 import com.maxpoliakov.skillapp.R
+import com.maxpoliakov.skillapp.databinding.CollapsingToolbarBinding
 import com.maxpoliakov.skillapp.databinding.SkilldetailFragBinding
 import com.maxpoliakov.skillapp.model.mapToPresentation
-import com.maxpoliakov.skillapp.ui.MainActivity
-import com.maxpoliakov.skillapp.ui.common.ActionBarFragment
+import com.maxpoliakov.skillapp.ui.common.FragmentWithCollapsingToolbar
 import com.maxpoliakov.skillapp.util.charts.setup
 import com.maxpoliakov.skillapp.util.fragment.observe
 import com.maxpoliakov.skillapp.util.fragment.showTimePicker
 import com.maxpoliakov.skillapp.util.lifecycle.viewModels
 import com.maxpoliakov.skillapp.util.ui.navigateAnimated
-import com.maxpoliakov.skillapp.util.ui.setMarginTop
 import com.maxpoliakov.skillapp.util.ui.setState
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @AndroidEntryPoint
-class SkillDetailFragment : ActionBarFragment(R.menu.skilldetail_frag_menu) {
+class SkillDetailFragment : FragmentWithCollapsingToolbar(R.menu.skilldetail_frag_menu) {
     private val viewModel by viewModels { viewModelFactory.create(args.skillId) }
 
     @Inject
     lateinit var viewModelFactory: SkillDetailViewModel.Factory
 
     private lateinit var binding: SkilldetailFragBinding
+
+    override val collapsingToolbarBinding: CollapsingToolbarBinding
+        get() = binding.collapsingToolbarLayout
 
     private val args: SkillDetailFragmentArgs by navArgs()
 
@@ -55,31 +54,6 @@ class SkillDetailFragment : ActionBarFragment(R.menu.skilldetail_frag_menu) {
         binding.productivityChart.chart.setup()
         observe(viewModel.statsChartData, binding.productivityChart.chart::setState)
         observe(viewModel.showRecordDialog) { showRecordDialog() }
-    }
-
-    override fun onStart() = binding.collapsingToolbarLayout.run {
-        super.onStart()
-        appBar.post {
-            appBar.setMarginTop(-collapsingToolbar.height)
-        }
-
-        lifecycleScope.launch {
-            awaitUntilAnimationFinished()
-            (requireActivity() as MainActivity).setToolbar(collapsingToolbar)
-            appBar.setMarginTop(0)
-        }
-
-        Unit
-    }
-
-    private suspend fun awaitUntilAnimationFinished() {
-        val animationDuration = requireContext().resources.getInteger(R.integer.animation_duration)
-        delay(animationDuration.toLong())
-    }
-
-    override fun onStop() = binding.collapsingToolbarLayout.run {
-        super.onStop()
-        (requireActivity() as MainActivity).resetToolbar()
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {

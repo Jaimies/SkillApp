749fe9ed522d29543e5029501f461828f0652df6
Maksym Poliakov <maxpoliakov.git@gmail.com>
2021-08-14 17:24:14 +0300

    ui: improve the pie chart design

--- app/src/main/java/com/maxpoliakov/skillapp/util/charts/PieChart.kt
+++ app/src/main/java/com/maxpoliakov/skillapp/util/charts/PieChart.kt
@@ -2,24 +2,35 @@ package com.maxpoliakov.skillapp.util.charts
 
 import android.graphics.Color
 import com.github.mikephil.charting.charts.PieChart
+import com.github.mikephil.charting.components.Legend
 import com.github.mikephil.charting.data.PieData
 import com.github.mikephil.charting.data.PieDataSet
 import com.github.mikephil.charting.data.PieEntry
 import com.github.mikephil.charting.utils.ColorTemplate
+import com.maxpoliakov.skillapp.util.ui.getColorAttributeValue
 
 fun PieChart.setup() {
     setHoleColor(Color.TRANSPARENT)
     holeRadius = 52.5f
     transparentCircleRadius = 0f
     description.isEnabled = false
-    legend.isEnabled = false
+
+    legend.run {
+        textColor = context.getColorAttributeValue(android.R.attr.textColorPrimary)
+        setDrawInside(false)
+        verticalAlignment = Legend.LegendVerticalAlignment.BOTTOM
+        horizontalAlignment = Legend.LegendHorizontalAlignment.LEFT
+        orientation = Legend.LegendOrientation.VERTICAL
+    }
+
+    setExtraOffsets(0f, 0f, 0f, 20f)
 }
 
 fun PieChart.setData(entries: List<PieEntry>) {
     val dataSet = PieDataSet(entries, "").apply {
-        setColors(ColorTemplate.COLORFUL_COLORS, 255)
+        setColors(ColorTemplate.COLORFUL_COLORS + ColorTemplate.MATERIAL_COLORS, 255)
         sliceSpace = 15f
-
+        setDrawEntryLabels(false)
         setDrawValues(false)
     }
 

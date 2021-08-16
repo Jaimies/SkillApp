5665852d1dcc217a3b890db1861be6439831018e
Maksym Poliakov <maxpoliakov.git@gmail.com>
2021-08-16 18:20:09 +0300

    fix: unable to drag a skill out of a group when dragging downwards

--- app/src/main/java/com/maxpoliakov/skillapp/ui/skills/EmptyDelegateAdapter.kt
+++ app/src/main/java/com/maxpoliakov/skillapp/ui/skills/EmptyDelegateAdapter.kt
@@ -7,7 +7,10 @@ import com.maxpoliakov.skillapp.ui.common.adapter.DelegateAdapter
 
 class EmptyDelegateAdapter : DelegateAdapter<Nothing, RecyclerView.ViewHolder> {
     override fun onCreateViewHolder(parent: ViewGroup): RecyclerView.ViewHolder {
-        return EmptyViewHolder(View(parent.context))
+        val view = View(parent.context).apply {
+            layoutParams = ViewGroup.LayoutParams(parent.width, 1)
+        }
+        return EmptyViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, item: Nothing) {}

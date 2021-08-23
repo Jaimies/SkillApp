6975b89608af6873ddc71b4d18466775fd836513
Maksym Poliakov <maxpoliakov.git@gmail.com>
2021-08-23 17:31:22 +0300

    fix: EditableViewModelTest

--- app/src/test/java/com/maxpoliakov/skillapp/ui/addskill/EditableViewModelTest.kt
+++ app/src/test/java/com/maxpoliakov/skillapp/ui/addskill/EditableViewModelTest.kt
@@ -1,14 +1,19 @@
 package com.maxpoliakov.skillapp.ui.addskill
 
 import com.maxpoliakov.skillapp.setupThreads
+import com.maxpoliakov.skillapp.ui.common.EditableViewModel
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.shouldBe
+
+class EditableViewModelImpl : EditableViewModel() {
+    override fun save(name: String) {}
+}
 
 class SkillViewModelTest : StringSpec({
     beforeSpec { setupThreads() }
 
     "inputIsValid" {
-        val viewModel = SkillViewModel()
+        val viewModel = EditableViewModelImpl()
         viewModel.inputIsValid.observeForever {}
         viewModel.name.value = "New name"
         viewModel.inputIsValid.value shouldBe true

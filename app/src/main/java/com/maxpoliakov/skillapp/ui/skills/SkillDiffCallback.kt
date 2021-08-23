1033de0c9a77b0172382952a485dd12f5d41589f
Maksym Poliakov <maxpoliakov.git@gmail.com>
2021-08-23 19:15:29 +0300

    fix(SkillDiffCallback): properly diff SKillGroupFooters

--- app/src/main/java/com/maxpoliakov/skillapp/ui/skills/SkillDiffCallback.kt
+++ app/src/main/java/com/maxpoliakov/skillapp/ui/skills/SkillDiffCallback.kt
@@ -10,11 +10,13 @@ class SkillDiffCallback : DiffUtil.ItemCallback<Any>() {
         return oldItem is StopwatchUiModel && newItem is StopwatchUiModel
                 || oldItem is Skill && newItem is Skill && oldItem.id == newItem.id
                 || oldItem is SkillGroup && newItem is SkillGroup && newItem.id == oldItem.id
+                || oldItem is SkillGroupFooter && newItem is SkillGroupFooter && newItem.groupId == oldItem.groupId
     }
 
     override fun areContentsTheSame(oldItem: Any, newItem: Any): Boolean {
         return oldItem is StopwatchUiModel && newItem is StopwatchUiModel
                 || oldItem is Skill && newItem is Skill && oldItem.copy(order = 0) == newItem.copy(order = 0)
                 || oldItem is SkillGroup && newItem is SkillGroup && newItem == oldItem
+                || oldItem is SkillGroupFooter && newItem is SkillGroupFooter
     }
 }
--- app/src/test/java/com/maxpoliakov/skillapp/ui/skills/DiffCallbackTest.kt
+++ app/src/test/java/com/maxpoliakov/skillapp/ui/skills/DiffCallbackTest.kt
@@ -1,34 +1,53 @@
 package com.maxpoliakov.skillapp.ui.skills
 
 import com.maxpoliakov.skillapp.domain.model.Skill
+import com.maxpoliakov.skillapp.domain.model.SkillGroup
 import com.maxpoliakov.skillapp.ui.skills.stopwatch.StopwatchUiModel
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.shouldBe
 import java.time.Duration.ZERO
 import java.time.LocalDate
 
-
 class DiffCallbackTest : StringSpec({
     val callback = SkillDiffCallback()
 
-    "areItemsTheSame" {
+    "areItemsTheSame() with the same type of item" {
         callback.areItemsTheSame(createSkill(1, "B"), createSkill(1, "A")) shouldBe true
         callback.areItemsTheSame(createSkill(1, "A"), createSkill(2, "A")) shouldBe false
+        callback.areItemsTheSame(createGroup(1, "A"), createGroup(1, "B")) shouldBe true
+        callback.areItemsTheSame(createGroup(2, "A"), createGroup(1, "A")) shouldBe false
+        callback.areItemsTheSame(SkillGroupFooter(2), SkillGroupFooter(1)) shouldBe false
+        callback.areItemsTheSame(SkillGroupFooter(2), SkillGroupFooter(2)) shouldBe true
+    }
+
+    "areItemsTheSame() always returns false if the types of items are different" {
         callback.areItemsTheSame(StopwatchUiModel, StopwatchUiModel) shouldBe true
         callback.areItemsTheSame(StopwatchUiModel, createSkill(1, "A")) shouldBe false
+        callback.areItemsTheSame(StopwatchUiModel, createGroup(1, "A")) shouldBe false
+        callback.areItemsTheSame(createSkill(1, "A"), createGroup(1, "A")) shouldBe false
+        callback.areItemsTheSame(StopwatchUiModel, createSkill(1, "A")) shouldBe false
+        callback.areItemsTheSame(StopwatchUiModel, createGroup(1, "A")) shouldBe false
+        callback.areItemsTheSame(createSkill(1, "A"), createGroup(1, "A")) shouldBe false
     }
 
     "areContentsTheSame" {
         callback.areContentsTheSame(createSkill(1, "A"), createSkill(1, "A")) shouldBe true
         callback.areContentsTheSame(createSkill(1, "A"), createSkill(1, "B")) shouldBe false
+        callback.areContentsTheSame(createGroup(1, "A"), createGroup(1, "A")) shouldBe true
+        callback.areContentsTheSame(createGroup(1, "A"), createGroup(1, "B")) shouldBe false
         callback.areContentsTheSame(StopwatchUiModel, StopwatchUiModel) shouldBe true
+        callback.areContentsTheSame(SkillGroupFooter(1), SkillGroupFooter(1)) shouldBe true
     }
 
-    "contents are the same if order is order is different" {
+    "contents are the same if order is different" {
         callback.areContentsTheSame(createSkill(0, "A", 2), createSkill(0, "A", 3))
     }
 })
 
-private fun createSkill(id: Int, name: String, order: Int = 0) : Skill {
+private fun createSkill(id: Int, name: String, order: Int = 0): Skill {
     return Skill(name, ZERO, ZERO, ZERO, id, LocalDate.ofEpochDay(0), order)
+}
+
+private fun createGroup(id: Int, name: String, order: Int = 0): SkillGroup {
+    return SkillGroup(id, name, listOf(), order)
 }

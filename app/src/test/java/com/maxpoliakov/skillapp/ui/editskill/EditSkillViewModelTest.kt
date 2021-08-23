5559820959a74169178cf292d83b8d598bd8abf8
Maksym Poliakov <maxpoliakov.git@gmail.com>
2021-08-23 17:29:39 +0300

    fix: EditSkillViewModelTest

--- app/src/test/java/com/maxpoliakov/skillapp/ui/editskill/EditSkillViewModelTest.kt
+++ app/src/test/java/com/maxpoliakov/skillapp/ui/editskill/EditSkillViewModelTest.kt
@@ -2,8 +2,8 @@ package com.maxpoliakov.skillapp.ui.editskill
 
 import com.maxpoliakov.skillapp.domain.usecase.skill.UpdateSkillUseCase
 import com.maxpoliakov.skillapp.model.SkillItem
-import com.maxpoliakov.skillapp.model.mapToDomain
 import com.maxpoliakov.skillapp.setupThreads
+import com.maxpoliakov.skillapp.util.navigation.NavigationUtil
 import io.kotest.core.spec.style.StringSpec
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers.Unconfined
@@ -14,19 +14,22 @@ import java.time.LocalDate
 
 class EditSkillViewModelTest : StringSpec({
     lateinit var useCase: UpdateSkillUseCase
+    lateinit var navigationUtil: NavigationUtil
     lateinit var viewModel: EditSkillViewModel
 
     beforeSpec { setupThreads() }
 
     beforeEach {
         useCase = mock(UpdateSkillUseCase::class.java)
-        viewModel = EditSkillViewModel(useCase, createSkillItem(), CoroutineScope(Unconfined))
+        navigationUtil = mock(NavigationUtil::class.java)
+
+        viewModel = EditSkillViewModel(useCase, navigationUtil, createSkillItem(), CoroutineScope(Unconfined))
     }
 
     "updateSkill()" {
         viewModel.name.value = "New Name    "
-        viewModel.updateSkill()
-        verify(useCase).run(createSkillItem("New Name").mapToDomain())
+        viewModel.update()
+        verify(useCase).updateName(0, "New name")
     }
 })
 

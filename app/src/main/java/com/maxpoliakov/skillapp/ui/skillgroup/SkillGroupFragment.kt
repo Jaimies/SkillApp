1867e855964fc1b0def81df7e0adca64d48b90fc
Maksym Poliakov <maxpoliakov.git@gmail.com>
2021-08-24 10:07:39 +0300

    ui(skill group bar chart): show skills ordered by their time

--- app/src/main/java/com/maxpoliakov/skillapp/ui/skillgroup/SkillGroupFragment.kt
+++ app/src/main/java/com/maxpoliakov/skillapp/ui/skillgroup/SkillGroupFragment.kt
@@ -52,7 +52,7 @@ class SkillGroupFragment : FragmentWithCollapsingToolbar(R.menu.skilldetail_frag
         observe(viewModel.group) { group ->
             val data = group.skills.map { skill ->
                 PieEntry(skill.totalTime.toMillis().toFloat() / group.totalTime.toMillis(), skill.name)
-            }
+            }.sortedBy { -it.y }
 
             binding.splitChart.chart.setData(data)
         }

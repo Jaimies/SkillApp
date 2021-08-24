4f8c55b2d26f59183c7988ae24f2eb315f17da8f
Maksym Poliakov <maxpoliakov.git@gmail.com>
2021-08-24 19:08:05 +0300

    chore: update deps

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -4,16 +4,16 @@ object Versions {
     const val coroutines = "1.5.0"
 
     const val androidxCore = "1.6.0"
-    const val appcompat = "1.3.0"
-    const val constraintLayout = "2.0.4"
+    const val appcompat = "1.3.1"
+    const val constraintLayout = "2.1.0"
     const val legacy = "1.0.0"
 
     const val room = "2.3.0"
-    const val paging = "3.0.0"
+    const val paging = "3.0.1"
     const val navigation = "2.3.5"
     const val lifecycle = "2.3.1"
     const val livedata = "2.2.0"
-    const val fragment = "1.3.5"
+    const val fragment = "1.3.6"
 
     const val material = "1.4.0"
     const val MPAndroidChart = "3.1.0"
@@ -25,8 +25,8 @@ object Versions {
     const val lint = "26.5.3"
     const val gradle = "4.2.2"
 
-    const val mockito = "3.2.4"
-    const val kotestJUnit5 = "4.6.0"
-    const val androidxTestRunner = "1.3.0"
-    const val androidJUnit = "1.1.2"
+    const val mockito = "3.12.1"
+    const val kotestJUnit5 = "4.6.1"
+    const val androidxTestRunner = "1.4.0"
+    const val androidJUnit = "1.1.3"
 }

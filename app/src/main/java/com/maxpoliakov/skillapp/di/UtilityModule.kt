29e089e0695fb429d934ce23d07ab5d6a6f1c5f9
Maksym Poliakov <maxpoliakov.git@gmail.com>
2021-09-07 17:27:28 +0300

    refactor(di): group util modules into a single one

--- app/src/main/java/com/maxpoliakov/skillapp/di/UtilityModule.kt
+++ app/src/main/java/com/maxpoliakov/skillapp/di/UtilityModule.kt
@@ -1,5 +1,7 @@
 package com.maxpoliakov.skillapp.di
 
+import com.maxpoliakov.skillapp.util.notifications.NotificationUtil
+import com.maxpoliakov.skillapp.util.notifications.NotificationUtilImpl
 import com.maxpoliakov.skillapp.util.stopwatch.StopwatchPersistence
 import com.maxpoliakov.skillapp.util.stopwatch.StopwatchPersistenceImpl
 import com.maxpoliakov.skillapp.util.stopwatch.StopwatchUtil
@@ -11,10 +13,13 @@ import dagger.hilt.components.SingletonComponent
 
 @Module
 @InstallIn(SingletonComponent::class)
-interface StopwatchModule {
+interface UtilityModule {
     @Binds
     fun provideStopwatchUtil(stopwatchUtil: StopwatchUtilImpl): StopwatchUtil
 
     @Binds
     fun provideStopwatchPersistence(persistence: StopwatchPersistenceImpl): StopwatchPersistence
+
+    @Binds
+    fun provideNotificationUtil(notificationUtil: NotificationUtilImpl): NotificationUtil
 }

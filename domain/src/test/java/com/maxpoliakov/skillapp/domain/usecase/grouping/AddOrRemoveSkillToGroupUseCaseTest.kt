b5b0f9c65d884a5daaeee6c79e89fdc4d98d5cb8
Maksym Poliakov <maxpoliakov.git@gmail.com>
2021-09-18 19:13:04 +0300

    test: cover AddOrRemoveSkillToGroupUseCase

---
+++ domain/src/test/java/com/maxpoliakov/skillapp/domain/usecase/grouping/AddOrRemoveSkillToGroupUseCaseTest.kt
@@ -0,0 +1,36 @@
+package com.maxpoliakov.skillapp.domain.usecase.grouping
+
+import com.maxpoliakov.skillapp.domain.model.Skill
+import com.maxpoliakov.skillapp.domain.model.SkillGroup
+import com.maxpoliakov.skillapp.domain.repository.SkillGroupRepository
+import io.kotest.core.spec.style.StringSpec
+import io.mockk.coEvery
+import io.mockk.coVerify
+import io.mockk.mockk
+import java.time.Duration
+
+class AddOrRemoveSkillToGroupUseCaseTest : StringSpec({
+    "deletes group if it becomes empty" {
+        val (useCase, skillGroupRepository) = createUseCase()
+        val skill = Skill("skill", Duration.ZERO, Duration.ZERO, groupId = 1)
+
+        useCase.addToGroup(skill, 1)
+        coVerify { skillGroupRepository.deleteGroup(1) }
+    }
+
+    "doesn't delete group if it still has skills" {
+        val (useCase, skillGroupRepository) = createUseCase()
+        val skill = Skill("skill", Duration.ZERO, Duration.ZERO, groupId = -1)
+
+        useCase.addToGroup(skill, 1)
+        coVerify(exactly = 0) { skillGroupRepository.deleteGroup(1) }
+    }
+})
+
+fun createUseCase(): Pair<AddOrRemoveSkillToGroupUseCase, SkillGroupRepository> {
+    val skillGroupRepository = mockk<SkillGroupRepository>(relaxed = true)
+
+    coEvery { skillGroupRepository.getSkillGroupById(any()) } returns SkillGroup(1, "name", listOf(), -1)
+
+    return AddOrRemoveSkillToGroupUseCase(skillGroupRepository) to skillGroupRepository
+}

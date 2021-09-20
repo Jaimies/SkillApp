7b9a227c75b3a7dd8ea37484a576e81cb3e7aa66
Maksym Poliakov <maxpoliakov.git@gmail.com>
2021-09-20 14:53:27 +0300

    ui(date formatting): show time in 24h mode

--- app/src/main/java/com/maxpoliakov/skillapp/util/time/DateTimeFormatter.kt
+++ app/src/main/java/com/maxpoliakov/skillapp/util/time/DateTimeFormatter.kt
@@ -2,4 +2,4 @@ package com.maxpoliakov.skillapp.util.time
 
 import java.time.format.DateTimeFormatter
 
-val dateTimeFormatter = DateTimeFormatter.ofPattern("MMM dd, yyyy  hh:mm:ss")
+val dateTimeFormatter: DateTimeFormatter = DateTimeFormatter.ofPattern("MMM dd, yyyy  HH:mm:ss")

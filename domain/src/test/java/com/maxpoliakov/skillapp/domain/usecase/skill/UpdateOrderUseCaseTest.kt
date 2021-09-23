314a71947fc92c54d274c55bf550839ae6f584b6
Maksym Poliakov <maxpoliakov.git@gmail.com>
2021-09-23 22:36:58 +0300

    test: cover UpdateOrderUseCase

---
+++ domain/src/test/java/com/maxpoliakov/skillapp/domain/usecase/skill/UpdateOrderUseCaseTest.kt
@@ -0,0 +1,48 @@
+package com.maxpoliakov.skillapp.domain.usecase.skill
+
+import com.maxpoliakov.skillapp.domain.repository.SkillGroupRepository
+import com.maxpoliakov.skillapp.domain.repository.SkillRepository
+import com.maxpoliakov.skillapp.domain.usecase.createGroup
+import com.maxpoliakov.skillapp.domain.usecase.createSkill
+import io.kotest.core.spec.style.StringSpec
+import io.mockk.coVerifyAll
+import io.mockk.mockk
+
+class UpdateOrderUseCaseTest : StringSpec({
+    "works" {
+        val skillRepository = mockk<SkillRepository>(relaxed = true)
+        val groupRepository = mockk<SkillGroupRepository>(relaxed = true)
+
+        val useCase = UpdateOrderUseCase(skillRepository, groupRepository)
+
+        val items = listOf(
+            createSkill(id = 1, groupId = -1),
+            createSkill(id = 2, groupId = 1),
+            createSkill(id = 3, groupId = 2),
+            createSkill(id = 4, groupId = -1),
+            createSkill(id = 5, groupId = 1),
+            createGroup(
+                id = 1,
+                skills = listOf(),
+            ),
+            createGroup(
+                id = 2,
+                skills = listOf(),
+            ),
+        )
+
+        useCase.run(items)
+
+        coVerifyAll {
+            skillRepository.updateOrder(1, 0)
+            skillRepository.updateOrder(4, 1)
+
+            groupRepository.updateOrder(1, 2)
+            groupRepository.updateOrder(2, 3)
+
+            skillRepository.updateOrder(2, 0)
+            skillRepository.updateOrder(5, 1)
+            skillRepository.updateOrder(3, 0)
+        }
+    }
+})
---
+++ domain/src/test/java/com/maxpoliakov/skillapp/domain/usecase/testData.kt
@@ -0,0 +1,13 @@
+package com.maxpoliakov.skillapp.domain.usecase
+
+import com.maxpoliakov.skillapp.domain.model.Skill
+import com.maxpoliakov.skillapp.domain.model.SkillGroup
+import java.time.Duration
+
+fun createSkill(id: Int = -1, groupId: Int): Skill {
+    return Skill("name", Duration.ZERO, Duration.ZERO, id = id, groupId = groupId)
+}
+
+fun createGroup(id: Int = -1, skills: List<Skill> = listOf()): SkillGroup {
+    return SkillGroup(id, "group", skills, -1)
+}

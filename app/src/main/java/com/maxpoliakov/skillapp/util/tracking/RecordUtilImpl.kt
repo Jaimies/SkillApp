0d5a91594a7330fcf0d3ea56ad56728d85754783
Maksym Poliakov <maxpoliakov.git@gmail.com>
2021-11-21 21:07:49 +0200

    ui: change the snackbar action text color

--- app/src/main/java/com/maxpoliakov/skillapp/util/tracking/RecordUtilImpl.kt
+++ app/src/main/java/com/maxpoliakov/skillapp/util/tracking/RecordUtilImpl.kt
@@ -6,6 +6,7 @@ import com.maxpoliakov.skillapp.R
 import com.maxpoliakov.skillapp.domain.model.Record
 import com.maxpoliakov.skillapp.domain.usecase.records.ChangeRecordTimeUseCase
 import com.maxpoliakov.skillapp.util.fragment.showTimePicker
+import com.maxpoliakov.skillapp.util.ui.getColorAttributeValue
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -18,6 +19,7 @@ class RecordUtilImpl @Inject constructor(
     override fun notifyRecordAdded(view: View, record: Record) {
         val snackbar = Snackbar.make(view, R.string.record_added, Snackbar.LENGTH_LONG)
         snackbar.setAction(R.string.change_time) { editTime(view, record) }
+        snackbar.setActionTextColor(view.context.getColorAttributeValue(R.attr.snackbarActionTextColor))
         snackbar.show()
     }
 

aee04ed8f372ebdcc7c0c3e60b45bebdfb306acf
Maksym Poliakov <maxpoliakov.git@gmail.com>
2022-01-27 11:30:40 +0200

    refactor(NumberPickerView): don't use reflection when setting Typeface

--- app/src/main/java/com/maxpoliakov/skillapp/util/ui/NumberPicker.kt
+++ app/src/main/java/com/maxpoliakov/skillapp/util/ui/NumberPicker.kt
@@ -1,14 +1,10 @@
 package com.maxpoliakov.skillapp.util.ui
 
 import android.graphics.Typeface
-import android.text.TextPaint
 import cn.carbswang.android.numberpickerview.library.NumberPickerView
 
 fun NumberPickerView.setFontFamily(fontFamily: String) {
-    val field = this::class.java.getDeclaredField("mPaintText")
-    field.isAccessible = true
-    val paint = field.get(this) as TextPaint
-    paint.typeface = Typeface.create(fontFamily, Typeface.NORMAL)
+    setContentTextTypeface(Typeface.create(fontFamily, Typeface.NORMAL))
 }
 
 fun NumberPickerView.setup(displayedValues: Array<String>) {

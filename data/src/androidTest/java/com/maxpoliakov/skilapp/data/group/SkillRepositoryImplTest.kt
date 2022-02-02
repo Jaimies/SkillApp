106a2845aaadc45b4ea225052f70cc56eeb02831
Maksym Poliakov <maxpoliakov.git@gmail.com>
2022-02-02 20:16:50 +0200

    test: cover the logic for updating goals

---
+++ data/src/androidTest/java/com/maxpoliakov/skilapp/data/group/SkillRepositoryImplTest.kt
@@ -0,0 +1,58 @@
+package com.maxpoliakov.skilapp.data.group
+
+import com.maxpoliakov.skilapp.data.createTestDatabase
+import com.maxpoliakov.skillapp.data.db.AppDatabase
+import com.maxpoliakov.skillapp.data.group.SkillGroupRepositoryImpl
+import com.maxpoliakov.skillapp.data.records.RecordsDao
+import com.maxpoliakov.skillapp.data.skill.SkillDao
+import com.maxpoliakov.skillapp.data.skill.SkillRepositoryImpl
+import com.maxpoliakov.skillapp.domain.model.Goal
+import com.maxpoliakov.skillapp.domain.model.Skill
+import com.maxpoliakov.skillapp.domain.model.SkillGroup
+import com.maxpoliakov.skillapp.domain.repository.SkillGroupRepository
+import io.kotest.matchers.shouldBe
+import kotlinx.coroutines.runBlocking
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+import java.time.Duration
+
+class SkillRepositoryImplTest {
+    private lateinit var db: AppDatabase
+    private lateinit var skillDao: SkillDao
+    private lateinit var recordsDao: RecordsDao
+    private lateinit var groupRepository: SkillGroupRepository
+    private lateinit var skillRepository: SkillRepositoryImpl
+
+    @Before
+    fun setup() {
+        db = createTestDatabase()
+        skillDao = db.skillDao()
+        recordsDao = db.recordsDao()
+        groupRepository = SkillGroupRepositoryImpl(db.skillGroupDao())
+        skillRepository = SkillRepositoryImpl(db.skillDao())
+    }
+
+    @After
+    fun teardown() {
+        db.close()
+    }
+
+    @Test
+    fun skillGroupRepository_updateGoal() = runBlocking {
+        groupRepository.createGroup(SkillGroup(1, "new group", listOf(), null, 0))
+        groupRepository.updateGoal(1, goal)
+        groupRepository.getSkillGroupById(1)?.goal shouldBe goal
+    }
+
+    @Test
+    fun skillRepository_updateGoal() = runBlocking {
+        skillRepository.addSkill(Skill("", Duration.ZERO, Duration.ZERO, goal = null))
+        skillRepository.updateGoal(1, goal)
+        skillRepository.getSkillById(1)?.goal shouldBe goal
+    }
+
+    companion object {
+        private val goal = Goal(Duration.ofHours(3), Goal.Type.Weekly)
+    }
+}

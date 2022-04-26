caba8cb39b6eb9b10dc6820149789337de6344d2
Maksym Poliakov <maxpoliakov.git@gmail.com>
2022-04-26 21:29:24 +0300

    fix: unable to a skill beyond the skill group footer

--- app/src/main/java/com/maxpoliakov/skillapp/ui/skills/SkillGroupFooterDelegateAdapter.kt
+++ app/src/main/java/com/maxpoliakov/skillapp/ui/skills/SkillGroupFooterDelegateAdapter.kt
@@ -8,7 +8,7 @@ import com.maxpoliakov.skillapp.ui.common.adapter.DelegateAdapter
 class SkillGroupFooterDelegateAdapter : DelegateAdapter<SkillGroupFooter, SkillGroupFooterViewHolder> {
     override fun onCreateViewHolder(parent: ViewGroup): SkillGroupFooterViewHolder {
         val view = View(parent.context).apply {
-            layoutParams = ViewGroup.LayoutParams(parent.width, 1)
+            layoutParams = ViewGroup.LayoutParams(parent.width, 10)
         }
         return SkillGroupFooterViewHolder(view)
     }

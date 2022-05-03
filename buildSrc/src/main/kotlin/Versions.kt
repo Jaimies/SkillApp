509a4886d24aa479cd561eda4719feb4b0e09739
Maksym Poliakov <maxpoliakov.git@gmail.com>
2022-05-03 23:39:09 +0200

    chore: update deps

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,6 +1,6 @@
 @Suppress("unused")
 object Versions {
-    const val kotlin = "1.6.10"
+    const val kotlin = "1.6.21"
     const val coroutines = "1.6.0"
 
     const val androidxCore = "1.7.0"
@@ -9,24 +9,24 @@ object Versions {
     const val legacy = "1.0.0"
 
     const val room = "2.3.0"
-    const val paging = "3.1.0"
-    const val navigation = "2.4.1"
-    const val lifecycle = "2.4.0"
+    const val paging = "3.1.1"
+    const val navigation = "2.4.2"
+    const val lifecycle = "2.4.1"
     const val livedata = "2.2.0"
     const val fragment = "1.4.1"
     const val workManager = "2.7.1"
-    const val billing = "4.0.0"
+    const val billing = "4.1.0"
 
     const val googleHttpClient = "1.40.0"
     const val googleApiClient = "1.32.1"
     const val googleApiServices = "v3-rev197-1.25.0"
-    const val playServicesAuth = "20.1.0"
+    const val playServicesAuth = "20.2.0"
 
     const val material = "1.5.0"
     const val preference = "1.2.0"
     const val MPAndroidChart = "3.1.0"
 
-    const val hilt = "2.40.5"
+    const val hilt = "2.41"
 
     const val firebase = "28.2.0"
 

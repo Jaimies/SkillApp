e97371a32793d4f0aad9929c54b8532f5e352121
Maksym Poliakov <maxpoliakov.git@gmail.com>
2022-05-03 23:37:47 +0200

    fix: AddSkillViewModelTest

--- app/src/test/java/com/maxpoliakov/skillapp/ui/addskill/AddSkillViewModelTest.kt
+++ app/src/test/java/com/maxpoliakov/skillapp/ui/addskill/AddSkillViewModelTest.kt
@@ -1,27 +1,48 @@
 package com.maxpoliakov.skillapp.ui.addskill
 
+import com.android.billingclient.api.PurchasesUpdatedListener
+import com.android.billingclient.api.SkuDetails
+import com.maxpoliakov.skillapp.data.billing.ExtendedBillingRepository
 import com.maxpoliakov.skillapp.domain.model.MeasurementUnit
 import com.maxpoliakov.skillapp.domain.model.Skill
+import com.maxpoliakov.skillapp.domain.repository.BillingRepository
 import com.maxpoliakov.skillapp.domain.usecase.skill.AddSkillUseCase
 import com.maxpoliakov.skillapp.setupThreads
 import com.maxpoliakov.skillapp.test.any
 import io.kotest.core.spec.style.StringSpec
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.flowOf
 import org.mockito.Mockito.`when`
 import org.mockito.Mockito.mock
 import org.mockito.Mockito.verify
 import java.time.Duration
 import java.time.Duration.ZERO
+import java.time.LocalDateTime
+
+class StubBillingRepository: ExtendedBillingRepository {
+    override fun addOneTimePurchaseUpdateListener(listener: PurchasesUpdatedListener) {}
+    override suspend fun getSubscriptionSkuDetails(): SkuDetails? = null
+
+    override val subscriptionState = MutableStateFlow(BillingRepository.SubscriptionState.Subscribed)
+    override val connectionState = BillingRepository.ConnectionState.Connected
+
+    override suspend fun connect() {}
+    override suspend fun getSubscriptionExpirationTime(): LocalDateTime? = null
+}
 
 class AddSkillViewModelTest : StringSpec({
     lateinit var viewModel: AddSkillViewModel
     lateinit var useCase: AddSkillUseCase
+    lateinit var billingRepository: ExtendedBillingRepository
 
     beforeSpec { setupThreads() }
 
     beforeEach {
         useCase = mock(AddSkillUseCase::class.java)
         `when`(useCase.run(any())).thenReturn(1)
-        viewModel = AddSkillViewModel(useCase)
+        billingRepository = StubBillingRepository()
+        viewModel = AddSkillViewModel(useCase, billingRepository)
     }
 
     "addSkill()" {

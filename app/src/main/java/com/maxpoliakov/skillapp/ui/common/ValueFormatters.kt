d0bd3b507f8b56a06011789303b59785d66ca293
Maksym Poliakov <maxpoliakov.git@gmail.com>
2022-05-23 12:53:10 +0200

    ui(bar chart): show year number when displaying monthly stats

--- app/src/main/java/com/maxpoliakov/skillapp/ui/common/ValueFormatters.kt
+++ app/src/main/java/com/maxpoliakov/skillapp/ui/common/ValueFormatters.kt
@@ -1,13 +1,9 @@
 package com.maxpoliakov.skillapp.ui.common
 
-import android.content.Context
 import com.github.mikephil.charting.formatter.ValueFormatter
-import com.maxpoliakov.skillapp.R
 import com.maxpoliakov.skillapp.shared.util.EPOCH
 import com.maxpoliakov.skillapp.shared.util.atStartOfWeek
 import com.maxpoliakov.skillapp.shared.util.shortName
-import com.maxpoliakov.skillapp.util.time.toReadableFloat
-import java.time.Duration
 
 class DayFormatter : ValueFormatter() {
     override fun getFormattedValue(value: Float): String {
@@ -26,6 +22,6 @@ class WeekFormatter : ValueFormatter() {
 class MonthFormatter : ValueFormatter() {
     override fun getFormattedValue(value: Float): String {
         val date = EPOCH.plusMonths(value.toLong())
-        return "\n${date.month.shortName}"
+        return "${date.month.shortName}\n${date.year % 100}"
     }
 }

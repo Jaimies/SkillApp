39d62c63ea60e0dbda448b93325c702b04202363
Maksym Poliakov <maxpoliakov.git@gmail.com>
2022-10-16 14:55:46 +0200

    refactor(SkillSelectionCriteria): make isValid a regular function rather than a suspend function

--- domain/src/main/java/com/maxpoliakov/skillapp/domain/model/SkillSelectionCriteria.kt
+++ domain/src/main/java/com/maxpoliakov/skillapp/domain/model/SkillSelectionCriteria.kt
@@ -2,16 +2,16 @@ package com.maxpoliakov.skillapp.domain.model
 
 sealed class SkillSelectionCriteria {
     object Any : SkillSelectionCriteria() {
-        override suspend fun isValid(skill: Skill) = true
+        override fun isValid(skill: Skill) = true
     }
 
     class WithId(private val id: Id) : SkillSelectionCriteria() {
-        override suspend fun isValid(skill: Skill) = this.id == skill.id
+        override fun isValid(skill: Skill) = this.id == skill.id
     }
 
     class InGroupWithId(private val groupId: Id) : SkillSelectionCriteria() {
-        override suspend fun isValid(skill: Skill) = skill.groupId == this.groupId
+        override fun isValid(skill: Skill) = skill.groupId == this.groupId
     }
 
-    abstract suspend fun isValid(skill: Skill): Boolean
+    abstract fun isValid(skill: Skill): Boolean
 }

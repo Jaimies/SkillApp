dccd8efea32db529b42b9c87c74b3bc828dd0217
Maksym Poliakov <maxpoliakov.git@gmail.com>
2022-10-27 09:24:34 +0200

    ui(intro): make a proper slide for entering the edit mode

--- app/src/main/java/com/maxpoliakov/skillapp/ui/intro/FirstRunIntro.kt
+++ app/src/main/java/com/maxpoliakov/skillapp/ui/intro/FirstRunIntro.kt
@@ -9,15 +9,9 @@ class FirstRunIntro : BaseIntro() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        if (shouldShowTheIntroSlide())
-            addSlide(R.string.intro_slide1_title, R.string.intro_slide1_description)
-
+        addSlide(R.string.intro_slide1_title, R.string.intro_slide1_description, "intro_reorder")
         addSlide(R.string.intro_slide2_title, R.string.intro_slide2_description, "intro_grouping")
         addSlide(R.string.intro_slide3_title, R.string.intro_slide3_description, "intro_units")
         addSlide(R.string.intro_slide4_title, R.string.intro_slide4_description, "intro_backups")
-    }
-
-    private fun shouldShowTheIntroSlide(): Boolean {
-        return LocalDate.now() < LocalDate.of(2022, Month.AUGUST, 15)
     }
 }

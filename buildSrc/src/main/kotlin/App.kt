d331fb802f7a13cc3ca9b1f2151a7232d410b5dc
Maksym Poliakov <maxpoliakov.git@gmail.com>
2022-10-29 22:47:08 +0200

    chore: release 3.1.0

--- buildSrc/src/main/kotlin/App.kt
+++ buildSrc/src/main/kotlin/App.kt
@@ -4,7 +4,7 @@ object App {
     const val targetSdk = 31
     const val minSdk = 21
     const val buildTools = "30.0.3"
-    const val versionCode = 1
-    const val versionName = "1.0.0"
+    const val versionCode = 149
+    const val versionName = "3.1.0"
     const val applicationId = "com.maxpoliakov.skillapp"
 }

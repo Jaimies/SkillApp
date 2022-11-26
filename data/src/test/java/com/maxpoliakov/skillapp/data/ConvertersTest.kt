1f804302a883b37aad01a5a2734a496232fc8a86
Maksym Poliakov <maxpoliakov.git@gmail.com>
2022-11-26 14:43:49 +0100

    refactor(ConvertersTest): more precise tests for fromDateRange() and toDateRange()

--- data/src/test/java/com/maxpoliakov/skillapp/data/ConvertersTest.kt
+++ data/src/test/java/com/maxpoliakov/skillapp/data/ConvertersTest.kt
@@ -10,13 +10,24 @@ import java.time.LocalDateTime
 class ConvertersTest : StringSpec({
     val epoch = LocalDate.ofEpochDay(0)
 
-    "toDateRange() and fromDateRange()" {
-        listOf(
+    "fromDateRange()" {
+        Converters.fromDateRange(
             LocalDateTime.parse("2015-02-15T05:20:30")..LocalDateTime.parse("2020-05-25T20:30:00"),
+        ) shouldBe "2015-02-15T05:20:30|2020-05-25T20:30:00"
+
+        Converters.fromDateRange(
             LocalDateTime.parse("2022-11-01T13:15:15")..LocalDateTime.parse("2022-11-15T02:40:55"),
-        ).forEach { range ->
-            Converters.toDateRange(Converters.fromDateRange(range)) shouldBe range
-        }
+        ) shouldBe "2022-11-01T13:15:15|2022-11-15T02:40:55"
+    }
+
+    "toDateRange()" {
+        Converters.toDateRange(
+            "2015-02-15T05:20:30|2020-05-25T20:30:00",
+        ) shouldBe LocalDateTime.parse("2015-02-15T05:20:30")..LocalDateTime.parse("2020-05-25T20:30:00")
+
+        Converters.toDateRange(
+            "2022-11-01T13:15:15|2022-11-15T02:40:55",
+        ) shouldBe LocalDateTime.parse("2022-11-01T13:15:15")..LocalDateTime.parse("2022-11-15T02:40:55")
     }
 
     "toDateRange() returns null if value cannot be parsed" {

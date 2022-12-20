7285a7dae6dd9f3170b99c30c7102c32f18f37a1
Maksym Poliakov <maxpoliakov.git@gmail.com>
2022-12-20 15:50:34 +0100

    refactor: move LocalDateAsStringSerializer and LocalTimeAsStringSerializer into their own files

--- data/src/main/java/com/maxpoliakov/skillapp/data/serialization/LocalTimeAsStringSerializer.kt
+++ data/src/main/java/com/maxpoliakov/skillapp/data/serialization/LocalTimeAsStringSerializer.kt
@@ -5,26 +5,8 @@ import kotlinx.serialization.descriptors.PrimitiveKind
 import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
 import kotlinx.serialization.encoding.Decoder
 import kotlinx.serialization.encoding.Encoder
-import java.time.LocalDate
-import java.time.LocalDateTime
 import java.time.LocalTime
 import java.time.format.DateTimeFormatter
-
-object LocalDateAsStringSerializer : KSerializer<LocalDate> {
-    private val formatter = DateTimeFormatter.ISO_LOCAL_DATE
-
-    override fun serialize(encoder: Encoder, value: LocalDate) {
-        val string = value.format(formatter)
-        encoder.encodeString(string)
-    }
-
-    override fun deserialize(decoder: Decoder): LocalDate {
-        val string = decoder.decodeString()
-        return formatter.parse(string, LocalDate::from)
-    }
-
-    override val descriptor = PrimitiveSerialDescriptor("LocalDateAsStringDescriptor", PrimitiveKind.STRING)
-}
 
 object LocalTimeAsStringSerializer: KSerializer<LocalTime> {
     private val formatter = DateTimeFormatter.ISO_LOCAL_TIME

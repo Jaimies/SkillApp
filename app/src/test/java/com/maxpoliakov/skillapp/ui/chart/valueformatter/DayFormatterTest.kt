03f4a5a44546fa04a1a4228cae52cf37db47a66f
Maksym Poliakov <maxpoliakov.git@gmail.com>
2022-12-30 02:29:59 +0100

    refactor: move the chart-related code from com.maxpoliakov.skillapp.ui.common to com.maxpoliakov.skillapp.chart

---
+++ app/src/test/java/com/maxpoliakov/skillapp/ui/chart/valueformatter/DayFormatterTest.kt
@@ -0,0 +1,13 @@
+package com.maxpoliakov.skillapp.ui.chart.valueformatter
+
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
+
+class DayFormatterTest : StringSpec({
+    "getFormattedValue()" {
+        val formatter = DayFormatter()
+        formatter.getFormattedValue(0f) shouldBe "1\nJan"
+        formatter.getFormattedValue(1f) shouldBe "2\nJan"
+        formatter.getFormattedValue(4f) shouldBe "5\nJan"
+    }
+})
--- app/src/test/java/com/maxpoliakov/skillapp/ui/chart/valueformatter/TimeFormatterTest.kt
+++ app/src/test/java/com/maxpoliakov/skillapp/ui/chart/valueformatter/TimeFormatterTest.kt
@@ -1,12 +1,12 @@
-package com.maxpoliakov.skillapp.ui.common
+package com.maxpoliakov.skillapp.ui.chart.valueformatter
 
 import com.maxpoliakov.skillapp.createMockContext
 import com.maxpoliakov.skillapp.model.UiMeasurementUnit
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.shouldBe
 
-class ValueFormattersTest : StringSpec({
-    "TimeFormatter" {
+class TimeFormatterTest : StringSpec({
+    "getFormattedValue()" {
         val formatter = UiMeasurementUnit.Millis.getValueFormatter(createMockContext())
         formatter.getFormattedValue(0f) shouldBe ""
         formatter.getFormattedValue(1_000f) shouldBe "1m"
@@ -14,12 +14,5 @@ class ValueFormattersTest : StringSpec({
         formatter.getFormattedValue(120_000f) shouldBe "2m"
         formatter.getFormattedValue(3_600_000f) shouldBe "1h"
         formatter.getFormattedValue(3_840_000f) shouldBe "1.1h"
-    }
-
-    "WeekDayFormatter" {
-        val formatter = DayFormatter()
-        formatter.getFormattedValue(0f) shouldBe "1\nJan"
-        formatter.getFormattedValue(1f) shouldBe "2\nJan"
-        formatter.getFormattedValue(4f) shouldBe "5\nJan"
     }
 })

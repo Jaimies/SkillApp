826a0b222b6c89bf587bc5b5aa9fc6c4a0b81c7e
Maksym Poliakov <maxpoliakov.git@gmail.com>
2023-01-18 22:10:57 +0100

    fix(IntegerValuePicker): create the 'numberOfValues' values, rather than 5000 no matter the value of 'numberOfValues'

--- app/src/main/java/com/maxpoliakov/skillapp/ui/common/picker/IntegerValuePicker.kt
+++ app/src/main/java/com/maxpoliakov/skillapp/ui/common/picker/IntegerValuePicker.kt
@@ -1,5 +1,6 @@
 package com.maxpoliakov.skillapp.ui.common.picker
 
+import android.os.Bundle
 import com.maxpoliakov.skillapp.model.UiMeasurementUnit
 
 abstract class IntegerValuePicker : ValuePicker() {
@@ -7,20 +8,34 @@ abstract class IntegerValuePicker : ValuePicker() {
 
     abstract val unit: UiMeasurementUnit
 
-    override fun getFirstPickerValues() = Array(5000) { index ->
+    override fun getFirstPickerValues() = Array(getNumberOfValues()) { index ->
         unit.toLongString(index.toLong(), requireContext())
+    }
+
+    private fun getNumberOfValues(): Int {
+        return requireArguments().getLong(MAXIMUM_VALUE, 5_000L).toInt()
     }
 
     override fun getSecondPickerValues() = arrayOf<String>()
 
     abstract class Builder : ValuePicker.Builder() {
-        open val maximumValue: Long = 5_000L
+        open val numberOfValues = 5_000
+        private val maxValue get() = numberOfValues - 1
 
         override var secondPickerEnabled = false
 
         override fun setCount(count: Long): Builder {
-            setFirstPickerValue(count.coerceAtMost(maximumValue).toInt())
+            setFirstPickerValue(count.toInt().coerceAtMost(maxValue))
             return this
         }
+
+        override fun saveArguments(bundle: Bundle) {
+            super.saveArguments(bundle)
+            bundle.putInt(MAXIMUM_VALUE, numberOfValues)
+        }
+    }
+
+    companion object {
+        const val MAXIMUM_VALUE = "MAXIMUM_VALUE"
     }
 }

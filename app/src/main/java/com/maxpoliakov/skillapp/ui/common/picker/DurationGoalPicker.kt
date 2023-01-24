a63541202c0f18d3073eab5395e3fd1c474acc84
Maksym Poliakov <maxpoliakov.git@gmail.com>
2023-01-24 22:03:48 +0100

    ui(DurationGoalPicker): add smaller weekly values: 15m, 30m, and 45m

--- app/src/main/java/com/maxpoliakov/skillapp/ui/common/picker/DurationGoalPicker.kt
+++ app/src/main/java/com/maxpoliakov/skillapp/ui/common/picker/DurationGoalPicker.kt
@@ -5,10 +5,6 @@ import com.maxpoliakov.skillapp.domain.model.MeasurementUnit
 import java.time.Duration
 
 class DurationGoalPicker : GoalPicker<Duration>(MeasurementUnit.Millis, goalValues) {
-    override fun getWeeklyPickerValue(value: Duration): String {
-        return requireContext().getString(R.string.time_hours, value.toHours().toString())
-    }
-
     class Builder : GoalPicker.Builder<Duration>(MeasurementUnit.Millis, goalValues) {
         override fun createDialog() = DurationGoalPicker()
     }
@@ -17,7 +13,8 @@ class DurationGoalPicker : GoalPicker<Duration>(MeasurementUnit.Millis, goalValu
         private val emptyGoalValues = arrayOf(Duration.ZERO)
         private val dailyGoalValues =
             Array(12) { index -> Duration.ofMinutes(index * 5L) } + Array(24) { index -> Duration.ofHours(index + 1L) }
-        private val weeklyGoalValues = Array(169) { index -> Duration.ofHours(index.toLong()) }
+        private val weeklyGoalValues =
+            Array(4) { index -> Duration.ofMinutes(index * 15L) } + Array(168) { index -> Duration.ofHours(index + 1L) }
         private val goalValues = arrayOf(emptyGoalValues, dailyGoalValues, weeklyGoalValues)
     }
 }

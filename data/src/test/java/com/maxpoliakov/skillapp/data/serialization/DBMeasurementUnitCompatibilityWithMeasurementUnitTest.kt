e7232f95b1b21485f7e0b203efada1e9e5f92c1d
Maksym Poliakov <maxpoliakov.git@gmail.com>
2023-01-24 01:36:26 +0100

    test: make sure old values of MeasurementUnit are mapped correctly

---
+++ data/src/test/java/com/maxpoliakov/skillapp/data/serialization/DBMeasurementUnitCompatibilityWithMeasurementUnitTest.kt
@@ -0,0 +1,15 @@
+package com.maxpoliakov.skillapp.data.serialization
+
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.json.Json
+
+class DBMeasurementUnitCompatibilityWithMeasurementUnitTest : StringSpec({
+    "old values of MeasurementUnit are mapped to correct DBMeasurementUnit values" {
+        Json.decodeFromString<DBMeasurementUnit>("\"Millis\"") shouldBe DBMeasurementUnit.Millis
+        Json.decodeFromString<DBMeasurementUnit>("\"Meters\"") shouldBe DBMeasurementUnit.Meters
+        Json.decodeFromString<DBMeasurementUnit>("\"Times\"") shouldBe DBMeasurementUnit.Times
+        Json.decodeFromString<DBMeasurementUnit>("\"Pages\"") shouldBe DBMeasurementUnit.Pages
+    }
+})

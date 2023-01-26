d722dade400c55217402f6c09b6f0c6d7afced4b
Maksym Poliakov <maxpoliakov.git@gmail.com>
2023-01-26 21:39:59 +0100

    fix(AutoCompleteTextView::setup()): ensure the items are not filtered

--- app/src/main/java/com/maxpoliakov/skillapp/util/ui/AutoCompleteTextView.kt
+++ app/src/main/java/com/maxpoliakov/skillapp/util/ui/AutoCompleteTextView.kt
@@ -1,10 +1,12 @@
 package com.maxpoliakov.skillapp.util.ui
 
-import android.widget.ArrayAdapter
+import android.R
+import android.view.inputmethod.EditorInfo
 import android.widget.AutoCompleteTextView
 
 fun AutoCompleteTextView.setup(values: List<String>) {
-    val adapter = ArrayAdapter(context, android.R.layout.simple_dropdown_item_1line, values)
+    val adapter = NonFilteringArrayAdapter(context, R.layout.simple_dropdown_item_1line, values)
     setAdapter(adapter)
     setText(values[0], false)
+    inputType = EditorInfo.TYPE_NULL
 }

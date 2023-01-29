6ab7f18b16a1298e7397c7f8c668e0dc64e1763c
Maksym Poliakov <maxpoliakov.git@gmail.com>
2023-01-29 18:46:01 +0100

    refactor(StopwatchViewHolder): inject NavController instead of trying to find it through the Context

--- app/src/main/java/com/maxpoliakov/skillapp/ui/skills/stopwatch/StopwatchViewHolder.kt
+++ app/src/main/java/com/maxpoliakov/skillapp/ui/skills/stopwatch/StopwatchViewHolder.kt
@@ -1,8 +1,7 @@
 package com.maxpoliakov.skillapp.ui.skills.stopwatch
 
 import android.view.View
-import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
+import androidx.navigation.NavController
 import androidx.navigation.fragment.FragmentNavigatorExtras
 import com.maxpoliakov.skillapp.MainDirections
 import com.maxpoliakov.skillapp.R
@@ -10,7 +9,6 @@ import com.maxpoliakov.skillapp.databinding.StopwatchBannerBinding
 import com.maxpoliakov.skillapp.domain.model.Skill
 import com.maxpoliakov.skillapp.ui.common.BaseViewHolder
 import com.maxpoliakov.skillapp.util.tracking.RecordUtil
-import com.maxpoliakov.skillapp.util.ui.getBaseContext
 import dagger.assisted.Assisted
 import dagger.assisted.AssistedFactory
 import dagger.assisted.AssistedInject
@@ -18,8 +16,9 @@ import dagger.assisted.AssistedInject
 class StopwatchViewHolder @AssistedInject constructor(
     @Assisted
     private val binding: StopwatchBannerBinding,
-    private val recordUtil: RecordUtil,
     viewModel: StopwatchViewModel,
+    private val recordUtil: RecordUtil,
+    private val navController: NavController,
 ) : BaseViewHolder(binding) {
     init {
         binding.viewModel = viewModel
@@ -37,8 +36,6 @@ class StopwatchViewHolder @AssistedInject constructor(
         val directions = MainDirections.actionToSkillDetailFragment(skill.id)
         val transitionName = context.getString(R.string.skill_transition_name)
         val extras = FragmentNavigatorExtras(view to transitionName)
-        val activity = context.getBaseContext<AppCompatActivity>()
-        val navController = activity.findNavController(R.id.nav_host_container)
         navController.navigate(directions, extras)
     }
 

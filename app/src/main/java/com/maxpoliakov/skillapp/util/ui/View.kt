22503f6f6011eeb1843222ae3a5799a9c5c91eaf
Maksym Poliakov <maxpoliakov.git@gmail.com>
2023-01-29 18:47:40 +0100

    refactor: remove unused function Context.getBaseContext()

--- app/src/main/java/com/maxpoliakov/skillapp/util/ui/View.kt
+++ app/src/main/java/com/maxpoliakov/skillapp/util/ui/View.kt
@@ -1,15 +1,10 @@
 package com.maxpoliakov.skillapp.util.ui
 
-import android.content.Context
-import android.content.ContextWrapper
 import android.graphics.Rect
 import android.view.TouchDelegate
 import android.view.View
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-
-inline fun <reified T> Context.getBaseContext() =
-    if (this is T) this else (this as ContextWrapper).baseContext as T
 
 fun RecyclerView.setupAdapter(adapter: RecyclerView.Adapter<*>) {
     if (layoutManager != null && this.adapter != null)

fb72cdd4356b93e09191961e74302ac04787ab35
Maksym Poliakov <maxpoliakov.git@gmail.com>
2023-02-11 01:17:56 +0100

    refactor(RecordChange, RangeChange): use interface instead of object

--- domain/src/main/java/com/maxpoliakov/skillapp/domain/model/RangeChange.kt
+++ domain/src/main/java/com/maxpoliakov/skillapp/domain/model/RangeChange.kt
@@ -1,13 +1,13 @@
 package com.maxpoliakov.skillapp.domain.model
 
-object RangeChange {
-    data class Start<T : Comparable<T>>(val value: T) : Change<ClosedRange<T>> {
+interface RangeChange<T : Comparable<T>> : Change<ClosedRange<T>> {
+    data class Start<T : Comparable<T>>(val value: T) : RangeChange<T> {
         override fun apply(range: ClosedRange<T>): ClosedRange<T> {
             return value..range.endInclusive
         }
     }
 
-    data class End<T : Comparable<T>>(val value: T) : Change<ClosedRange<T>> {
+    data class End<T : Comparable<T>>(val value: T) : RangeChange<T> {
         override fun apply(range: ClosedRange<T>): ClosedRange<T> {
             return range.start..value
         }
--- domain/src/main/java/com/maxpoliakov/skillapp/domain/model/RecordChange.kt
+++ domain/src/main/java/com/maxpoliakov/skillapp/domain/model/RecordChange.kt
@@ -3,20 +3,20 @@ package com.maxpoliakov.skillapp.domain.model
 import java.time.LocalDate
 import java.time.LocalTime
 
-object RecordChange {
-    data class Date(val newDate: LocalDate) : Change<Record> {
+interface RecordChange : Change<Record> {
+    data class Date(val newDate: LocalDate) : RecordChange {
         override fun apply(record: Record): Record {
             return record.copy(date = newDate)
         }
     }
 
-    data class Count(val newCount: Long) : Change<Record> {
+    data class Count(val newCount: Long) : RecordChange {
         override fun apply(record: Record): Record {
             return record.copy(count = newCount)
         }
     }
 
-    data class TimeRange(val change: Change<ClosedRange<LocalTime>>) : Change<Record> {
+    data class TimeRange(val change: Change<ClosedRange<LocalTime>>) : RecordChange {
         override fun apply(record: Record): Record {
             if (record.timeRange == null) return record
             return record.copy(timeRange = change.apply(record.timeRange))

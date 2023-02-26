449940a36df6b8fcb1571220b7b37ceee3ea6f3f
Maksym Poliakov <maxpoliakov.git@gmail.com>
2023-02-26 02:13:43 +0100

    ui(number picker): increase scrolling speed

--- app/src/main/java/com/maxpoliakov/skillapp/util/ui/NumberPicker.kt
+++ app/src/main/java/com/maxpoliakov/skillapp/util/ui/NumberPicker.kt
@@ -1,10 +1,14 @@
 package com.maxpoliakov.skillapp.util.ui
 
+import android.os.Build
+import android.view.ViewConfiguration
 import android.widget.NumberPicker
+import org.lsposed.hiddenapibypass.HiddenApiBypass
+import java.lang.reflect.Field
 
 fun NumberPicker.setup(displayedValues: Array<String>) {
+    setupScrollSpeed()
     setValues(displayedValues)
-    setFriction(0.03f)
 }
 
 fun NumberPicker.setValues(displayedValues: Array<String>) {
@@ -16,6 +20,22 @@ fun NumberPicker.setValues(displayedValues: Array<String>) {
     invalidate()
 }
 
-fun setFriction(friction: Float) {
-    // todo
+private const val NUMBER_PICKER_MAX_FLING_VELOCITY_FIELD = "mMaximumFlingVelocity"
+
+fun NumberPicker.setupScrollSpeed() {
+    try {
+        val field = getDeclaredNonSdkField(NUMBER_PICKER_MAX_FLING_VELOCITY_FIELD)
+        field.isAccessible = true
+        field.setInt(this, ViewConfiguration.get(context).scaledMaximumFlingVelocity)
+    } catch (e: Exception) {
+    }
+}
+
+private fun Any.getDeclaredNonSdkField(fieldName: String): Field {
+    if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.R) {
+        return javaClass.getDeclaredField(fieldName)
+    } else {
+        return (HiddenApiBypass.getInstanceFields(javaClass) as List<Field>)
+            .first { it.name == fieldName }
+    }
 }

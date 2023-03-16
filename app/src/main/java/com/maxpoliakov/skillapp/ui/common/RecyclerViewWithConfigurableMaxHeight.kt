24e02883bc2966868384d34108dd1a2d3675c81d
Maksym Poliakov <maxpoliakov.git@gmail.com>
2023-03-16 15:32:52 +0100

    ui(details fragment): set maxHeight for the recyclerview rather than layout_height

---
+++ app/src/main/java/com/maxpoliakov/skillapp/ui/common/RecyclerViewWithConfigurableMaxHeight.kt
@@ -0,0 +1,33 @@
+package com.maxpoliakov.skillapp.ui.common
+
+import android.content.Context
+import android.util.AttributeSet
+import androidx.recyclerview.widget.RecyclerView
+import com.maxpoliakov.skillapp.R
+
+class RecyclerViewWithConfigurableMaxHeight : RecyclerView {
+    private var mMaxHeight = 0
+
+    constructor(context: Context) : super(context)
+    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs) {
+        initialize(context, attrs ?: return)
+    }
+
+    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {
+        initialize(context, attrs ?: return)
+    }
+
+    private fun initialize(context: Context, attrs: AttributeSet) {
+        val arr = context.obtainStyledAttributes(attrs, R.styleable.RecyclerViewWithConfigurableMaxHeight)
+        mMaxHeight = arr.getLayoutDimension(R.styleable.RecyclerViewWithConfigurableMaxHeight_maxHeight, mMaxHeight)
+        arr.recycle()
+    }
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        var heightMeasureSpec = heightMeasureSpec
+        if (mMaxHeight > 0) {
+            heightMeasureSpec = MeasureSpec.makeMeasureSpec(mMaxHeight, MeasureSpec.AT_MOST)
+        }
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+    }
+}

c49f3dfebf6f6edce021b4f65d3f4711531f7a0f
Maksym Poliakov <maxpoliakov.git@gmail.com>
2023-03-24 23:52:55 +0100

    fix(YearFormatter): wrong value for overridden property interval

--- app/src/main/java/com/maxpoliakov/skillapp/model/formatter/daterange/YearFormatter.kt
+++ app/src/main/java/com/maxpoliakov/skillapp/model/formatter/daterange/YearFormatter.kt
@@ -6,7 +6,7 @@ import com.maxpoliakov.skillapp.domain.model.StatisticInterval
 import java.time.LocalDate
 
 class YearFormatter : DateRangeFormatter() {
-    override val interval = StatisticInterval.Daily
+    override val interval = StatisticInterval.Yearly
     override val currentDateRangeStringResId = R.string.this_year
 
     override fun format(date: LocalDate): String {

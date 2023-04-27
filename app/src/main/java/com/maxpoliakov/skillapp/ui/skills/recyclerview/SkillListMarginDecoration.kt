fe974e75f388364d7179803616e0aa8ee7b61ff8
Maksym Poliakov <maxpoliakov.git@gmail.com>
2023-04-27 14:37:17 +0200

    refactor(skill group header): use margin instead of padding

--- app/src/main/java/com/maxpoliakov/skillapp/ui/skills/recyclerview/SkillListMarginDecoration.kt
+++ app/src/main/java/com/maxpoliakov/skillapp/ui/skills/recyclerview/SkillListMarginDecoration.kt
@@ -7,7 +7,7 @@ class SkillListMarginDecoration : BottomMarginItemDecoration() {
     override val marginByItemViewType = mapOf(
         SkillListAdapter.ItemType.Stopwatch to 20.dp,
         SkillListAdapter.ItemType.Skill to 25.dp,
-        SkillListAdapter.ItemType.SkillGroupHeader to 10.dp,
+        SkillListAdapter.ItemType.SkillGroupHeader to 20.dp,
         SkillListAdapter.ItemType.SkillGroupFooter to 24.dp,
     )
 }

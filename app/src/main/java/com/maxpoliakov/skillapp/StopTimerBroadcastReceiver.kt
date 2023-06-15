c1da81a22b40624630b28def1bf3fe3f6f168f58
Maksym Poliakov <maxpoliakov.git@gmail.com>
2023-06-15 13:49:47 +0200

    fix: use goAsync() for asynchronous operations in BroadcastReceivers

--- app/src/main/java/com/maxpoliakov/skillapp/StopTimerBroadcastReceiver.kt
+++ app/src/main/java/com/maxpoliakov/skillapp/StopTimerBroadcastReceiver.kt
@@ -3,13 +3,9 @@ package com.maxpoliakov.skillapp
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import com.maxpoliakov.skillapp.domain.di.ApplicationScope
-import com.maxpoliakov.skillapp.domain.repository.NotificationUtil
 import com.maxpoliakov.skillapp.domain.stopwatch.Stopwatch
+import com.maxpoliakov.skillapp.shared.extensions.goAsync
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @AndroidEntryPoint
@@ -17,14 +13,10 @@ class StopTimerBroadcastReceiver : BroadcastReceiver() {
     @Inject
     lateinit var stopwatch: Stopwatch
 
-    @Inject
-    @ApplicationScope
-    lateinit var scope: CoroutineScope
-
     override fun onReceive(context: Context?, intent: Intent?) {
         val skillId = intent?.getIntExtra("skillId", -1).takeIf { it != -1 } ?: return
 
-        scope.launch {
+        goAsync {
             stopwatch.stop(skillId)
         }
     }

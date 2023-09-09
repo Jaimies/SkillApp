36ff56b07f63176c4a52a420082d1cc245068192
Maksym Poliakov <maxpoliakov.git@gmail.com>
2023-09-09 22:22:57 +0200

    fix(TimerDao): ignore when trying to insert a timer with a duplicate primary key

--- data/src/main/java/com/maxpoliakov/skillapp/data/timer/TimerDao.kt
+++ data/src/main/java/com/maxpoliakov/skillapp/data/timer/TimerDao.kt
@@ -1,16 +1,20 @@
 package com.maxpoliakov.skillapp.data.timer
 
 import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
-import com.maxpoliakov.skillapp.data.db.BaseDao
-import com.maxpoliakov.skillapp.domain.model.Skill
 import kotlinx.coroutines.flow.Flow
 
 @Dao
-interface TimerDao: BaseDao<DBTimer> {
+interface TimerDao {
     @Query("SELECT * FROM timers")
     fun getAll(): Flow<List<DBTimer>>
 
-    @Query("DELETE FROM timers WHERE skillId = :skillId")
-    fun deleteBySkillId(skillId: Int)
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(timer: DBTimer): Long
+
+    @Delete
+    suspend fun delete(timer: DBTimer)
 }

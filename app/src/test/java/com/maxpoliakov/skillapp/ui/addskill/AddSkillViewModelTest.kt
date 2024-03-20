e89f2eb6f9a5fc7282026c761e39c466b0f7d30f
Maksym Poliakov <maxpoliakov.git@gmail.com>
2024-03-20 11:34:34 +0100

    test(AddSkillViewModelTest): fix flakiness by using mockk over mockito
    Verifying suspend function invocations using mockito may not be very reliable, mockk handles that better.

--- app/src/test/java/com/maxpoliakov/skillapp/ui/addskill/AddSkillViewModelTest.kt
+++ app/src/test/java/com/maxpoliakov/skillapp/ui/addskill/AddSkillViewModelTest.kt
@@ -5,11 +5,10 @@ import com.maxpoliakov.skillapp.domain.model.Skill
 import com.maxpoliakov.skillapp.domain.usecase.skill.ManageSkillUseCase
 import com.maxpoliakov.skillapp.resetThreads
 import com.maxpoliakov.skillapp.setupThreads
-import com.maxpoliakov.skillapp.test.any
 import io.kotest.core.spec.style.StringSpec
-import org.mockito.Mockito.`when`
-import org.mockito.Mockito.mock
-import org.mockito.Mockito.verify
+import io.mockk.coEvery
+import io.mockk.coVerify
+import io.mockk.mockk
 import java.time.Duration
 
 class AddSkillViewModelTest : StringSpec({
@@ -20,8 +19,8 @@ class AddSkillViewModelTest : StringSpec({
     afterSpec { resetThreads() }
 
     beforeEach {
-        useCase = mock(ManageSkillUseCase::class.java)
-        `when`(useCase.addSkill(any())).thenReturn(1)
+        useCase = mockk<ManageSkillUseCase>()
+        coEvery { useCase.addSkill(any()) } returns 1
         viewModel = AddSkillViewModel(useCase)
     }
 
@@ -29,20 +28,35 @@ class AddSkillViewModelTest : StringSpec({
         viewModel.totalTime.value = "100"
         viewModel.name.value = "Skill name "
         viewModel.update()
-        verify(useCase).addSkill(Skill("Skill name", MeasurementUnit.Millis, Duration.ofHours(100).toMillis(), Duration.ofHours(100).toMillis()))
+
+        coVerify {
+            useCase.addSkill(
+                Skill(
+                    "Skill name",
+                    MeasurementUnit.Millis,
+                    Duration.ofHours(100).toMillis(),
+                    Duration.ofHours(100).toMillis(),
+                )
+            )
+        }
     }
 
     "addSkill() uses ZERO as the time if the totalTime field is null" {
         viewModel.totalTime.value = null
         viewModel.name.value = "Name"
         viewModel.update()
-        verify(useCase).addSkill(Skill("Name", MeasurementUnit.Millis, 0, 0))
+
+        coVerify {
+            useCase.addSkill(Skill("Name", MeasurementUnit.Millis, 0, 0))
+        }
     }
 
     "addSkill() uses ZERO as the time if the totalTime field is empty" {
         viewModel.totalTime.value = ""
         viewModel.name.value = "Name"
         viewModel.update()
-        verify(useCase).addSkill(Skill("Name", MeasurementUnit.Millis, 0, 0))
+        coVerify {
+            useCase.addSkill(Skill("Name", MeasurementUnit.Millis, 0, 0))
+        }
     }
 })

64346306c500f862bd5e1e9427a11b8d5d922621
Maksym Poliakov <maxpoliakov.git@gmail.com>
2025-09-30 23:43:59 +0200

    refactor(buildSrc): remove unused constant buildTools

--- buildSrc/src/main/kotlin/App.kt
+++ buildSrc/src/main/kotlin/App.kt
@@ -3,7 +3,6 @@ object App {
     const val compileSdk = 35
     const val targetSdk = 35
     const val minSdk = 23
-    const val buildTools = "30.0.3"
     const val versionCode = 216
     const val versionName = "3.1.0"
     const val applicationId = "com.maxpoliakov.skillapp"

c1201bc8093202b71a2c3ef4bf3c2b8f02a6cfa0
Maksym Poliakov <maxpoliakov.git@gmail.com>
2025-09-30 15:41:32 +0200

    chore: increase targetsdk to 35

--- buildSrc/src/main/kotlin/App.kt
+++ buildSrc/src/main/kotlin/App.kt
@@ -1,7 +1,7 @@
 @Suppress("unused")
 object App {
-    const val compileSdk = 34
-    const val targetSdk = 34
+    const val compileSdk = 35
+    const val targetSdk = 35
     const val minSdk = 23
     const val buildTools = "30.0.3"
     const val versionCode = 216

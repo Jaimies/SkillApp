2902f48584f9f2fc36e1c8c3ce27b52d730aff62
Maksym Poliakov <maxpoliakov.git@gmail.com>
2025-10-01 12:43:34 +0200

    increase targetSdk to 36

--- buildSrc/src/main/kotlin/App.kt
+++ buildSrc/src/main/kotlin/App.kt
@@ -1,7 +1,7 @@
 @Suppress("unused")
 object App {
     const val compileSdk = 35
-    const val targetSdk = 35
+    const val targetSdk = 36
     const val minSdk = 23
     const val versionCode = 216
     const val versionName = "3.1.0"

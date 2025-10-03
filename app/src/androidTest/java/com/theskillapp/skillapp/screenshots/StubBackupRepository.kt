5f05bbbe1d2d7ad23c96f4411a0de842c6f1b623
Maksym Poliakov <maxpoliakov.git@gmail.com>
2025-10-03 16:01:42 +0200

    properly display last backup time in screenshots

--- app/src/androidTest/java/com/theskillapp/skillapp/screenshots/StubBackupRepository.kt
+++ app/src/androidTest/java/com/theskillapp/skillapp/screenshots/StubBackupRepository.kt
@@ -11,8 +11,12 @@ import javax.inject.Inject
 
 class StubBackupRepository @Inject constructor() : BackupRepository {
     override suspend fun save(data: BackupData) = Result.Success(Unit)
-    override suspend fun getBackups() = Result.Success(listOf<Backup>())
-    override suspend fun getLastBackup() = Result.Success(Backup(GenericUri("123abc"), LocalDateTime.now()))
+    override suspend fun getBackups() = Result.Success(listOf<Backup>(backup))
+    override suspend fun getLastBackup() = Result.Success(backup)
     override suspend fun getContents(backup: Backup) = Result.Success(BackupData(""))
-    override fun getLastBackupFlow() = flowOf<Result<Backup?>>()
+    override fun getLastBackupFlow() = flowOf(Result.Success(backup))
+
+    companion object {
+        private val backup = Backup(GenericUri("123abc"), LocalDateTime.now())
+    }
 }

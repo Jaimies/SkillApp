2e56aed27b2372a1201d9bf888780d98d6f04e29
Maksym Poliakov <maxpoliakov.git@gmail.com>
2025-10-04 00:47:50 +0200

    release version 4.0.0-beta02
    - remove translucency in the icon in the create backup button
    - try to prevent reproducibility issues with AboutLibraries

--- buildSrc/src/main/kotlin/App.kt
+++ buildSrc/src/main/kotlin/App.kt
@@ -3,7 +3,7 @@ object App {
     const val compileSdk = 35
     const val targetSdk = 36
     const val minSdk = 23
-    const val versionCode = 350
-    const val versionName = "4.0.0-beta01"
+    const val versionCode = 351
+    const val versionName = "4.0.0-beta02"
     const val applicationId = "com.theskillapp.skillapp"
 }

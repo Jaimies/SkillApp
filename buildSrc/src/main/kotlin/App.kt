ebc71c66121d7716ecc008743b7400c61c562a25
Maksym Poliakov <maxpoliakov.git@gmail.com>
2025-10-07 02:12:57 +0200

    chore: release version 4.0.2

--- buildSrc/src/main/kotlin/App.kt
+++ buildSrc/src/main/kotlin/App.kt
@@ -3,7 +3,7 @@ object App {
     const val compileSdk = 35
     const val targetSdk = 36
     const val minSdk = 23
-    const val versionCode = 353
-    const val versionName = "4.0.1"
+    const val versionCode = 354
+    const val versionName = "4.0.2"
     const val applicationId = "com.theskillapp.skillapp"
 }

34badbf5203eab5e95d39aacff1b74d285646c70
Maksym Poliakov <maxpoliakov.git@gmail.com>
2026-04-13 23:25:23 +0200

    chore: update dependencies (in particular, kotlin to 2.0)

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,24 +1,24 @@
 @Suppress("unused")
 object Versions {
-    const val kotlin = "1.9.23"
+    const val kotlin = "2.0.21"
     const val coroutines = "1.8.0"
     const val serialization = "1.3.3"
 
-    const val androidxCore = "1.9.0"
-    const val appcompat = "1.6.1"
+    const val androidxCore = "1.16.0"
+    const val appcompat = "1.7.1"
     const val constraintLayout = "2.1.4"
     const val androidxAnnotation = "1.3.0"
 
     const val room = "2.6.1"
     const val paging = "3.1.1"
-    const val navigation = "2.5.3"
+    const val navigation = "2.9.5"
     const val lifecycle = "2.6.1"
-    const val fragment = "1.5.6"
+    const val fragment = "1.8.9"
     const val activity = "1.8.2"
     const val workManager = "2.10.5"
     const val documentFile = "1.0.1"
 
-    const val material = "1.8.0"
+    const val material = "1.12.0"
     const val preference = "1.2.0"
     const val MPAndroidChart = "3.1.0"
 
